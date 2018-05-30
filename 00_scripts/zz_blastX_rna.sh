835ed66a074b805e7daf474b3ddf842dfeab0f4f
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-05-29 15:30:48 -1000

    Update zz_blastX_rna.sh

--- 00_scripts/zz_blastX_rna.sh
+++ 00_scripts/zz_blastX_rna.sh
@@ -1,7 +1,7 @@
 cd $SCRATCH
 cp /appli/bioinfo/blast/latest/blast.pbs .
 
-QUERY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/97_tests/GreenGEDBlastX.txt
+QUERY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/97_tests/GreenGEDBlastX.fasta
 BANK=/home/ref-bioinfo/beedeem/p/NCBI_nr/current/NCBI_nr/NCBI_nrM
 BLAST_PROGRAM=blastp
 BLAST_FORMAT=0
