e9dbad0a4fb93c8cdee1e3b22180ee91c421a86d
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-07-13 10:26:27 -1000

    Create zw_WGCNArnaSeq.sh

---
+++ 00_scripts/zw_WGCNArnaSeq.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#PBS -l walltime=20:00:00
+#PBS -l mem=100gb
+#PBS -N "wgcna"
+#PBS -o "log.wgcna.out"
+#PBS -l ncpus=2
+#PBS -q omp
+#PBS -r n
+
+cd $PBS_O_WORKDIR
+
+#Chargement de conda
+export PATH=$PATH:/appli/anaconda/2.7/bin
+
+#Chargement de votre environnement
+source activate /home1/scratch/pauffret/wgcna/src/test
+
+SCRIPT_WGCNA=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/zw_WGCNArnaSeq.R
+
+Rscript --vanilla $SCRIPT_WGCNA
