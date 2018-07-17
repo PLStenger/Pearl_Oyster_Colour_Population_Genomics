6dbb1eaaee5803aff5083d8ea08c31c03a81b915
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-07-16 14:29:05 -1000

    Update 06_flagstat.sh

--- 00_scripts/06_flagstat.sh
+++ 00_scripts/06_flagstat.sh
@@ -1,31 +1,21 @@
 #!/usr/bin/env bash
 #PBS -o flagstat.out
 #PBS -q omp
-#PBS -l ncpus=8
+#PBS -l ncpus=16
 #PBS -l mem=60gb
 #PBS -l walltime=96:00:00
-#PBS -r n
-
-# Header
-DATADIRECTORY=/home1/scratch/plstenge/testBWA/mapping_BWA_sspace.final.scaffolds.fasta
-DATAOUTPUT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/06_flagstat/HI.4499.006.NS_Adaptor_16.pool8KJ.txt
-SCRIPT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/06_flagstat
-HEADER=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/header.txt
-FLAGSTATENV=". /appli/bioinfo/samtools/latest/env.sh"
 
 
-#for FILE in $(ls $DATADIRECTORY/*.bam)
-#do
-#        cp $HEADER $SCRIPT/flagstat_${FILE##*/}.qsub ;
-#        echo "cd $DATADIRECTORY" >> $SCRIPT/flagstat_${FILE##*/}.qsub ;
-#        echo "$FLAGSTATENV"  >> $SCRIPT/flagstat_${FILE##*/}.qsub ;
-#        echo "samtools view $FILE | cut -f1 | sort | uniq | wc -l >> $DATAOUTPUT">> $SCRIPT/flagstat_${FILE##*/}.qsub ;
-#        
-#done ;
+#Global variables
+INPUT=/home1/scratch/plstenge/testBWA/mapping_BWA_sspace.final.scaffolds.fasta
+WORKING_DIRECTORY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/06_flagstat
+SCRIPT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts
+SAMTOOLS="samtools"
+SAM_ENV=". /appli/bioinfo/samtools/latest/env.sh"
+NB_CPU=16	#number of cpus
 
-#qsub $SCRIPT/flagstat_${FILE##*/}.qsub ;
 
-". /appli/bioinfo/samtools/latest/env.sh"
-
-samtools view /home1/scratch/plstenge/testBWA/mapping_BWA_sspace.final.scaffolds.fasta/HI.4499.006.NS_Adaptor_16.pool8KJ.bam | cut -f1 | sort | uniq | wc -l >> $DATAOUTPUT
-
+#loop through sam files:
+$SAM_ENV
+for f in $INPUT/*.bam; do $SAMTOOLS flagstat $f > ${f/.bam/.mapped_stat.txt};
+done
