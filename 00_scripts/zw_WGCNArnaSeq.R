d75e1ef93464956a9f160d813ab71a1f32ebd77a
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-07-20 13:48:11 -1000

    Update zw_WGCNArnaSeq.R

--- 00_scripts/zw_WGCNArnaSeq.R
+++ 00_scripts/zw_WGCNArnaSeq.R
@@ -13,7 +13,7 @@ library("stats", warn.conflicts = FALSE)
 
 # library("doParallel")
 
-ALLOW_WGCNA_THREADS=2
+#ALLOW_WGCNA_THREADS=2
 setwd("/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/97_tests")
 
 load("dataInput_subset.Rda")
@@ -25,7 +25,7 @@ load("sft_signed.Rda")
 # Test pour éviter ça : "Warning message:executing %dopar% sequentially: no parallel backend registered"
 # registerDoParallel()
 
-#allowWGCNAThreads(56)
+allowWGCNAThreads()
 
 #=====================================================================================
 #
