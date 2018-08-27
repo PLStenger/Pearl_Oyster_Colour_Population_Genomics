5264a84728a764dcd0f37795147aa5131e5763d5
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-08-27 12:56:38 -1000

    Create 07_flagstat_unique.sh

---
+++ 00_scripts/07_flagstat_unique.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#PBS -o flagstat.out
+#PBS -q omp
+#PBS -l ncpus=16
+#PBS -l mem=60gb
+#PBS -l walltime=96:00:00
+
+
+#Global variables
+INPUT=/home/datawork-rmpf/p_margaritifera/pl-pwgs/03_mapped
+SCRIPT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts
+SAMTOOLS="samtools"
+SAM_ENV=". /appli/bioinfo/samtools/latest/env.sh"
+NB_CPU=16	#number of cpus
+
+
+
+$SAM_ENV
+cd $INPUT
+
+$SAMTOOLS flagstat HI.4499.007.NS_Adaptor_15.pool7KR_filtered.bam > ${HI.4499.007.NS_Adaptor_15.pool7KR_filtered_flagstat.txt
