65894a4b43bfe2501e5d09f928cab20a925cc68b
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-08-28 08:47:10 -1000

    Update 08_filtering_bamfiles.sh

--- 00_scripts/08_filtering_bamfiles.sh
+++ 00_scripts/08_filtering_bamfiles.sh
@@ -42,7 +42,7 @@ cd $INPUT
 
 $SAMTOOLS_ENV
 
-$SAMTOOLS view -F 4 -F 256 -q 5 -f2 -b HI.4499.006.NS_Adaptor_16.pool8KJ.bam > HI.4499.006.NS_Adaptor_16.pool8KJ_filtered.bam
+$SAMTOOLS view -F 4 -F 256 -q 5 -f2 -b HI.4499.008.NS_Adaptor_14.pool6GV.bam > HI.4499.008.NS_Adaptor_14.pool6GV_filtered.bam
 
 
 
