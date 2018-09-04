57630f3c3beacc52af4bc84672025afc82d61d92
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-09-04 13:45:17 -1000

    Create 05_mapping_PE_bwa_unique.sh

---
+++ 00_scripts/05_mapping_PE_bwa_unique.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#PBS -q omp
+#PBS -l ncpus=16
+#PBS -l mem=100gb
+#PBS -l walltime=96:00:00
+
+#Global variables
+ASSEMBLY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds.fasta
+WORKING_DIRECTORY=/home1/scratch/plstenge/BWA/mapping_BWA_sspace.final.scaffolds.fasta
+SCRIPT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts
+BWA="bwa"			
+BWA_ENV=". /appli/bioinfo/bwa/latest/env.sh"
+
+#BWA
+
+cd ${WORKING_DIRECTORY}
+  
+${BWA_ENV}
+  
+time bwa mem -t 16 -M ${ASSEMBLY} /home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/03_trimmed/HI.4499.007.NS_Adaptor_15.pool7KR_R1.paired.fastq.gz /home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/03_trimmed/HI.4499.007.NS_Adaptor_15.pool7KR_R2.paired.fastq.gz > HI.4499.007.NS_Adaptor_15.pool7KR_05_mapping_unique.sam
+samtools view -b HI.4499.007.NS_Adaptor_15.pool7KR_05_mapping_unique.sam > HI.4499.007.NS_Adaptor_15.pool7KR_05_mapping_unique.bam
