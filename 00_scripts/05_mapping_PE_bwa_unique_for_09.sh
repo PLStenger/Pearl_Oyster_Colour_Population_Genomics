a4cff1374b5a08e750bb959244b647dd30b03875
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-09-04 16:59:54 -1000

    Create 05_mapping_PE_bwa_unique_for_09.sh

---
+++ 00_scripts/05_mapping_PE_bwa_unique_for_09.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#PBS -q omp
+#PBS -l walltime=70:00:00
+#PBS -l mem=600g
+#PBS -l ncpus=2
+
+#Global variables
+ASSEMBLY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds.fasta
+WORKING_DIRECTORY=/home1/scratch/plstenge/BWA/mapping_BWA_sspace.final.scaffolds.fasta
+SCRIPT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts
+BWA="bwa"			
+BWA_ENV=". /appli/bioinfo/bwa/latest/env.sh"
+
+#BWA
+
+cd ${WORKING_DIRECTORY}
+  
+${BWA_ENV}
+  
+time bwa mem -t 2 -M ${ASSEMBLY} /home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/03_trimmed/HI.4506.005.NS_Adaptor_9.pool1TR_R1.paired.fastq.gz /home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/03_trimmed/HI.4506.005.NS_Adaptor_9.pool1TR_R2.paired.fastq.gz > HI.4506.001.NS_Adaptor_13.pool5GJ_05_mapping_unique_sequentiel.sam
