5d0769a239a357c72ae67631d8a8ef296802b47a
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-09-04 17:02:42 -1000

    Update 05_mapping_PE_bwa_unique_for_09.sh

--- 00_scripts/05_mapping_PE_bwa_unique_for_09.sh
+++ 00_scripts/05_mapping_PE_bwa_unique_for_09.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-#PBS -q omp
-#PBS -l walltime=70:00:00
+#PBS -q sequentiel
+#PBS -l walltime=180:00:00
 #PBS -l mem=600g
-#PBS -l ncpus=2
 
 #Global variables
 ASSEMBLY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds.fasta
@@ -17,4 +16,4 @@ cd ${WORKING_DIRECTORY}
   
 ${BWA_ENV}
   
-time bwa mem -t 2 -M ${ASSEMBLY} /home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/03_trimmed/HI.4506.005.NS_Adaptor_9.pool1TR_R1.paired.fastq.gz /home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/03_trimmed/HI.4506.005.NS_Adaptor_9.pool1TR_R2.paired.fastq.gz > HI.4506.001.NS_Adaptor_13.pool5GJ_05_mapping_unique_sequentiel.sam
+time bwa mem -t 1 -M ${ASSEMBLY} /home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/03_trimmed/HI.4506.005.NS_Adaptor_9.pool1TR_R1.paired.fastq.gz /home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/03_trimmed/HI.4506.005.NS_Adaptor_9.pool1TR_R2.paired.fastq.gz > HI.4506.001.NS_Adaptor_13.pool5GJ_05_mapping_unique_sequentiel.sam
