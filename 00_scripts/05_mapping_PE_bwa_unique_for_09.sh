fff83b7655fe74fc6957fed3add736bc0453e10a
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-09-11 08:40:20 -1000

    Update 05_mapping_PE_bwa_unique_for_09.sh

--- 00_scripts/05_mapping_PE_bwa_unique_for_09.sh
+++ 00_scripts/05_mapping_PE_bwa_unique_for_09.sh
@@ -16,4 +16,4 @@ cd ${WORKING_DIRECTORY}
   
 ${BWA_ENV}
   
-time bwa mem -t 1 -M ${ASSEMBLY} /home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/03_trimmed/HI.4506.005.NS_Adaptor_9.pool1TR_R1.paired.fastq.gz /home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/03_trimmed/HI.4506.005.NS_Adaptor_9.pool1TR_R2.paired.fastq.gz > HI.4506.005.NS_Adaptor_9.pool1TR_05_mapping_unique_sequentiel.sam
+time bwa mem -t 28 -M ${ASSEMBLY} /home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/03_trimmed/HI.4506.005.NS_Adaptor_9.pool1TR_R1.paired.fastq.gz /home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/03_trimmed/HI.4506.005.NS_Adaptor_9.pool1TR_R2.paired.fastq.gz > HI.4506.005.NS_Adaptor_9.pool1TR_05_mapping_unique_sequentiel.sam
