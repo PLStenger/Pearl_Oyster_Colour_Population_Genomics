536aa9667ae7e9760d571ab0c0788735d3a301c9
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-09-12 14:32:04 -1000

    Update 09_sort_bamfiles_for_19.sh

--- 00_scripts/09_sort_bamfiles_for_19.sh
+++ 00_scripts/09_sort_bamfiles_for_19.sh
@@ -4,11 +4,12 @@
 #PBS -l select=1:ncpus=28:mem=115g
 
 #Global variables
+WORDING_DIRECTORY=/home1/scratch/plstenge/filtering_bamfiles
 SCRIPT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts
 SAMTOOLS_ENV=". /appli/bioinfo/samtools/latest/env.sh"
 SAMTOOLS="samtools"
 
-
+cd $WORDING_DIRECTORY
 $SAMTOOLS_ENV
 
 $SAMTOOLS sort /home/datawork-rmpf/p_margaritifera/pl-pwgs/03_mapped/HI.4527.005.NS_Adaptor_19.pool11EJ_filtered.bam > /home1/scratch/plstenge/filtering_bamfiles/HI.4527.005.NS_Adaptor_19.pool11EJ_filtered_sorted.bam
