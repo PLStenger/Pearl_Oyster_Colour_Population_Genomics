ea481005500e651176488bd7df54e824e7e81396
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-09-17 10:24:43 -1000

    Update 11_01_MarkDuplicates.sh

--- 00_scripts/11_01_MarkDuplicates.sh
+++ 00_scripts/11_01_MarkDuplicates.sh
@@ -3,46 +3,60 @@
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=28:mem=115g
 
-data=/home/datawork-rmpf/p_margaritifera/pl-pwgs/03_mapped
-outdir=/home/datawork-rmpf/p_margaritifera/pl-pwgs/05_variant_call_freebayes
-ref=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds.fasta
-pic=		#path to picard tools version 1.119
-tmp=/home1/scratch/plstenge/tmp # Path for temporary files
-samtools=". /appli/bioinfo/samtools/latest/env.sh"
-gatk=". /appli/bioinfo/gatk/latest/env.sh"
+DATA=/home/DATAwork-rmpf/p_margaritifera/pl-pwgs/03_mapped
+OUTDIR=/home/DATAwork-rmpf/p_margaritifera/pl-pwgs/05_variant_call_freebayes
+ASSEMBLY=/home1/DATAwork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_DATA/sspace.final.scaffolds.fasta
+PICARD_TOOLS=		#path to PICARD_TOOLSard tools version 1.119
+TMP=/home1/scratch/plstenge/tmp # Path for temporary files
+SAMTOOLS=". /appli/bioinfo/samtools/latest/env.sh"
+GATK=". /appli/bioinfo/gatk/latest/env.sh"
 
 file=__BASE__
 
-mkdir -p $outdir
+# 1) Marking duplicates and removing them
+cd ${DATA}
+time java -jar -Djava.io.TMPdir=$TMP ${PICARD_TOOLS}/MarkDuplicates.jar I=${file} O=${OUTDIR}/${file%.*}_MD.bam M=${OUTDIR}/${file%.*}_MD_metrics.txt ASSUME_SORTED=TRUE VALIDATION_STRINGENCY=SILENT REMOVE_DUPLICATES=TRUE CREATE_INDEX=TRUE ;
 
-#Marking duplicates and removing them
-cd ${data}
-time java -jar -Djava.io.tmpdir=$tmp ${pic}/MarkDuplicates.jar I=${file} O=${outdir}/${file%.*}_MD.bam M=${outdir}/${file%.*}_MD_metrics.txt ASSUME_SORTED=TRUE VALIDATION_STRINGENCY=SILENT REMOVE_DUPLICATES=true CREATE_INDEX=TRUE ;
+file=${OUTDIR}/${file%.*}_MD.bam
 
-file=${outdir}/${file%.*}_MD.bam
+# 2) Supplementary step to prevent bug: sorting & indexing bam files
+cd ${OUTDIR}
+$SAMTOOLS
+SAMTOOLS sort ${file} > ${file%.*}_MD_sorted.bam ;
+SAMTOOLS index ${file%.*}_MD_sorted.bam  > ${file%.*}_MD_sorted.bam.bai  ;
 
-#Sorting & indexing bam files
-cd ${outdir}
-$samtools
-samtools sort ${file} > ${file%.*}_sorted.bam ;
-samtools index ${file%.*}_sorted.bam  > ${file%.*}_sorted.bam.bai  ;
+# 3) Correctiong N cigar reads
+# This tool identifies all N cigar elements in sequence reads, and creates k+1 new reads 
+# (where k is the number of N cigar elements) that correspond to the segments of the original read beside/between 
+# the splicing events represented by the Ns in the original CIGAR.
+# The first read includes the bases that are to the left of the first N element, while the part of the read that is 
+# to the right of the N (including the Ns) is hard clipped, and so on for the rest of the new reads.
+$GATK
+time GATK SplitNCigarReads --TMP_DIR ${TMP} -R $ASSEMBLY -I ${file%.*}_MD_sorted.bam -O ${OUTDIR}/${file%.*}_sorted_split.bam ;
 
-#Correctiong N cigar reads
-$gatk
-time gatk SplitNCigarReads --TMP_DIR ${tmp} -R $ref -I ${file%.*}_sorted.bam -O ${file%.*}_sorted_split.bam ;
+# 4) Told it's pooling data
+# En fait, étape uniquement dans Freebayes ?
 
-#Add read group information to bam files
+# 5) Changing read's header avec PicardTools pour autoriser le pooling
+# AddOrReplaceReadGroups = Replace read groups in a BAM file.This tool enables the user to replace all read groups 
+# in the INPUT file with a single new read group and assign all reads to this read group in the OUTPUT BAM file.
+# RGID (String)	Read Group ID Default value: 1. This option can be set to 'null' to clear the default value.
+# RGLB (String)	Read Group library Required.
+# RGPL (String)	Read Group platform (e.g. illumina, solid) Required.
+# RGPU (String)	Read Group platform unit (eg. run barcode) Required.
+# RGSM (String)	Read Group sample name Required.
+# RGCN (String)	Read Group sequencing center name Default value: null.
+# RGDS (String)	Read Group description Default value: null.
+# RGDT (Iso8601Date)	Read Group run date Default value: null.
+# RGPI (Integer)	Read Group predicted insert size Default value: null.
+# RGPG (String)	Read Group program group Default value: null.
+# RGPM (String)	Read Group platform model Default value: null.
 id=${file##*/}
 id=${file%.*}
-time java -jar -Djava.io.tmpdir=$tmp ${pic}/AddOrReplaceReadGroups.jar I=${file%.*}_sorted_split.bam O=${file%.*}_sorted_split_RG.bam RGID=${id} RGLB=${id} RGPL=illumina RGPU=${id} RGSM=${id}
+time java -jar -Djava.io.TMPdir=$TMP ${PICARD_TOOLS}/AddOrReplaceReadGroups.jar I=${OUTDIR}/${file%.*}_sorted_split.bam O=${OUTDIR}/${file%.*}_sorted_split_RG.bam RGID=${id} RGLB=${id} RGPL=illumina RGPU=${id} RGSM=${id}
 
-#Sorting & indexing final bam files
-$samtools
-samtools index ${file%.*}_sorted_split_RG.bam > ${file%.*}_sorted_split_RG.bam.bai ;
-samtools flagstat ${file%.*}_sorted_split_RG.bam > ${file%.*}_sorted_split_RG.bam.flagstat ;
+# 6) Indexing bam
+$SAMTOOLS
+SAMTOOLS index ${OUTDIR}/${file%.*}_sorted_split_RG.bam > ${OUTDIR}/${file%.*}_sorted_split_RG.bam.bai ;
+SAMTOOLS flagstat ${OUTDIR}/${file%.*}_sorted_split_RG.bam > ${OUTDIR}/${file%.*}_sorted_split_RG.bam.flagstat ;
 
-#Removing int files
-rm ${file%.*}_sorted_split.bam
-rm ${file%.*}_sorted.bam
-rm ${file%.*}_sorted.bam.bai
-rm ${file}
