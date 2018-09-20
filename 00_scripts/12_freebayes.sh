39f0f91310c8e42040c016dc3e05df3f314191e4
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-09-20 13:02:08 -1000

    Update 12_freebayes.sh

--- 00_scripts/12_freebayes.sh
+++ 00_scripts/12_freebayes.sh
@@ -19,22 +19,24 @@
 
 FB_ENV=/home1/datawork/plstenge/96_env_conda/freebayes # path to conda freebayes env if needed Version 1.2.0
 REF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds.fasta # path to ref genome
-BAM=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/variants.txt		#path to txt file containing list of filtered bam files (resulting from step 03)
+BAM=/home1/scratch/plstenge/freebayes		#path to txt file containing list of filtered bam files (resulting from step 03)
 NB_CPU=12
 nAlleles=4
-minMapQ=20
-minCOV=5
-VCF=/home/datawork-rmpf/p_margaritifera/pl-pwgs/05_variant_call_freebayes		#path to output vcf file
-LOG=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/12_freebayes.out		#path to log file
+# minMapQ=20
+# minCOV=5
+# VCF=/home/datawork-rmpf/p_margaritifera/pl-pwgs/05_variant_call_freebayes		#path to output vcf file
+# LOG=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/12_freebayes.out		#path to log file
 
+cd ${BAM}
 ${FB_ENV}
 
-freebayes -f ${REF} \
-	--use-best-n-alleles $nAlleles \
-        --min-mapping-quality $minMapQ \
-	--pooled-discrete \
-        --min-coverage $minCOV \
-	--cnv-map cnv-map.bed \
-	--ploidy 2 \
-	--bam-list ${BAM} \
-	--vcf ${VCF} >& $LOG
+#freebayes -f ${REF} \
+#	--use-best-n-alleles $nAlleles \
+#        --min-mapping-quality $minMapQ \
+#	--pooled-discrete \
+#        --min-coverage $minCOV \
+#	--cnv-map sspace.final.scaffolds_Pool1_Pool2.bed \
+#	pool1.bam pool2.bam \
+#	--vcf ${VCF} >& $LOG
+	
+freebayes -f ${REF} --use-best-n-alleles $nAlleles --pooled-discrete --cnv-map sspace.final.scaffolds_Pool1_Pool2.bed --ploidy 2 pool1.bam pool2.bam | vcffilter -f "QUAL > 20" > pools1_2.vcf
