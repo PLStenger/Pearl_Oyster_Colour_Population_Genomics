71b838ec0aac74d5a1a97034601d176a5159d6ef
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-09-19 15:43:16 -1000

    Update 12_freebayes.sh

--- 00_scripts/12_freebayes.sh
+++ 00_scripts/12_freebayes.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
-#PBS -q sequentiel
-#PBS -l ncpus=1
-#PBS -l mem=60g
-#PBS -l walltime=12:00:00
+#PBS -q omp
+#PBS -l ncpus=12
+#PBS -l mem=100g
+#PBS -l walltime=96:00:00
 
-FB_ENV=/home1/datawork/plstenge/96_env_conda/freebayes # path to conda freebayes env if needed
-# FB=		#path to freebayes version v1.1.0-3-g961e5f3
+# FreeBayes is a Bayesian genetic variant detector designed to find small polymorphisms, specifically SNPs (single-nucleotide polymorphisms), 
+# indels (insertions and deletions), MNPs (multi-nucleotide polymorphisms), and complex events (composite insertion and substitution events) 
+# smaller than the length of a short-read sequencing alignment.
+
+# For pool datas:
+# --pooled-discrete
+# and
+# create a cnv.bed
+# multiple ploidy (here 2) by the nb of individuals
+
+# dans cnv-map.bed
+# mettre
+
+FB_ENV=/home1/datawork/plstenge/96_env_conda/freebayes # path to conda freebayes env if needed Version 1.2.0
 REF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds.fasta # path to ref genome
 BAM=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/variants.txt		#path to txt file containing list of filtered bam files (resulting from step 03)
 NB_CPU=12
@@ -20,6 +32,9 @@ ${FB_ENV}
 freebayes -f ${REF} \
 	--use-best-n-alleles $nAlleles \
         --min-mapping-quality $minMapQ \
+	--pooled-discrete \
         --min-coverage $minCOV \
+	--cnv-map cnv-map.bed \
+	--ploidy 2 \
 	--bam-list ${BAM} \
 	--vcf ${VCF} >& $LOG
