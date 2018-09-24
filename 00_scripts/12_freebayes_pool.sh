108b86899ccec739416bae46215805ae9f0e7557
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-09-24 08:26:13 -1000

    Update and rename 12_freebayes_test2.sh to 12_freebayes_pool.sh

--- 00_scripts/12_freebayes_pool.sh
+++ 00_scripts/12_freebayes_pool.sh
@@ -15,7 +15,10 @@ r/bin/env bash
 # multiple ploidy (here 2) by the nb of individuals
 
 # dans cnv-map.bed
-# mettre
+# mettre le meme header (e.g. "ID") pour chaque bam, et le nombre d'individu * la diploidie, soit:
+# /home/datawork-rmpf/p_margaritifera/pl-pwgs/05_variant_call_freebayes/HI.4499.006.NS_Adaptor_16.pool8KJ_filtered_sorted_MD	14
+# /home/datawork-rmpf/p_margaritifera/pl-pwgs/05_variant_call_freebayes/HI.4499.007.NS_Adaptor_15.pool7KR_filtered_sorted_MD	22
+# ici, HI.4499.006.NS_Adaptor_16.pool8KJ_filtered_sorted_MD à 7 individus.
 
 # FB_ENV=/home1/datawork/plstenge/96_env_conda/freebayes # path to conda freebayes env if needed Version 1.2.0
 REF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds.fasta # path to ref genome
