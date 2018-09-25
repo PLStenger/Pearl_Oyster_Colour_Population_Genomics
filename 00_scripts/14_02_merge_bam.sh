2db1cca9fec23a56feec7f40fe50b1c23db66cde
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-09-24 14:50:51 -1000

    Create 14_02_merge_bam.sh

---
+++ 00_scripts/14_02_merge_bam.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#PBS -q mpi
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=28:mem=115g
+
+#Global variables
+WORDING_DIRECTORY=/home1/scratch/plstenge/merged_bam
+SCRIPT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts
+SAMTOOLS_ENV=". /appli/bioinfo/samtools/latest/env.sh"
+SAMTOOLS="samtools"
+
+cd $WORDING_DIRECTORY
+$SAMTOOLS_ENV
+
