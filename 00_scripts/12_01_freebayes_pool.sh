8fde10bd7ce700148235cf0a84227644288dd4e1
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-10-04 14:10:35 -1000

    Update 12_01_freebayes_pool.sh

--- 00_scripts/12_01_freebayes_pool.sh
+++ 00_scripts/12_01_freebayes_pool.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #PBS -q omp
-#PBS -l walltime=180:00:00
-#PBS -l mem=300g
+#PBS -l walltime=680:00:00
+#PBS -l mem=800g
 #PBS -l ncpus=12
 
 # FreeBayes is a Bayesian genetic variant detector designed to find small polymorphisms, specifically SNPs (single-nucleotide polymorphisms), 
@@ -34,13 +34,5 @@ cd ${BAM}
 export PATH=$PATH:/appli/anaconda/2.7/bin
 source activate /home1/datawork/plstenge/96_env_conda/freebayes
 
-#freebayes -f ${REF} \
-#	--use-best-n-alleles $nAlleles \
-#        --min-mapping-quality $minMapQ \
-#	--pooled-discrete \
-#        --min-coverage $minCOV \
-#	--cnv-map sspace.final.scaffolds_Pool1_Pool2.bed \
-#	pool1.bam pool2.bam \
-#	--vcf ${VCF} >& $LOG
-	
-freebayes -f ${REF} --use-best-n-alleles $nAlleles --pooled-discrete --cnv-map sspace.final.scaffolds_Pool1_Pool2_test.bed pool1.bam pool2.bam > pools1_2test2.vcf
+
+freebayes -f ${REF} --use-best-n-alleles $nAlleles --pooled-discrete --cnv-map sspace.final.scaffolds_Pool1_Pool2_test.bed HI.4499.006.NS_Adaptor_16.pool8KJ_filtered_sorted_MD_sorted_split_RG.bam HI.4499.007.NS_Adaptor_15.pool7KR_filtered_sorted_MD_sorted_split_RG.bam HI.4499.008.NS_Adaptor_14.pool6GV_filtered_sorted_MD_sorted_split_RG.bam HI.4506.001.NS_Adaptor_13.pool5GJ_filtered_sorted_MD_sorted_split_RG.bam HI.4506.002.NS_Adaptor_12.pool4GR_filtered_sorted_MD_sorted_split_RG.bam HI.4506.003.NS_Adaptor_11.pool3TV_filtered_sorted_MD_sorted_split_RG.bam HI.4506.004.NS_Adaptor_10.pool2TJ_filtered_sorted_MD_sorted_split_RG.bam HI.4506.005.NS_Adaptor_9.pool1TR_filtered_sorted_MD_sorted_split_RG.bam HI.4527.004.NS_Adaptor_20.pool12EV_filtered_sorted_MD_MD_sorted.bam HI.4527.005.NS_Adaptor_19.pool11EJ_filtered_sorted_MD_sorted_split_RG.bam HI.4527.006.NS_Adaptor_18.pool10ER_filtered_sorted_MD_sorted_split_RG.bam HI.4527.007.NS_Adaptor_17.pool9KV_filtered_sorted_MD_sorted_split_RG.bam > individuals_genotypes.vcf
