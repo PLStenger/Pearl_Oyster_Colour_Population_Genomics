33dc4cbc24619b5373dd96f33d2179ea937019c7
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-10-10 08:50:04 -1000

    Update 16_freebayes_paralell_merged.sh

--- 00_scripts/16_freebayes_paralell_merged.sh
+++ 00_scripts/16_freebayes_paralell_merged.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #PBS -q omp
-#PBS -l walltime=180:00:00
-#PBS -l mem=800g
-#PBS -l ncpus=12
+#PBS -l walltime=120:00:00
+#PBS -l mem=115g
+#PBS -l ncpus=40
 
 REF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds.fasta # path to ref genome
 INDEX=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds.fasta.fai # path to ref genome index
 BAM=/home1/scratch/plstenge/merged_bam		
 nAlleles=4
-NCPU=12
+NCPU=40
 
 cd ${BAM}
 export PATH=$PATH:/appli/anaconda/2.7/bin
