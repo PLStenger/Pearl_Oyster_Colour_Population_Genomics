7d3e1f16cb1c07824ca33e6b496d799504093abf
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-10-16 09:02:38 -1000

    Create 12_03_freebayes_vcftools.sh

---
+++ 00_scripts/12_03_freebayes_vcftools.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#PBS -q mpi
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=28:mem=115g
+
+# Data from 12_01_freebayes_pool_no_genotypes.sh
+
+DATADIRECTORY=/home1/scratch/creisser/PL_BAM_MD
+VCFTOOLSENV=". /appli/bioinfo/vcftools/0.1.14/env.sh"
+
+$VCFTOOLSENV
+cd $DATADIRECTORY
+
+vcftools --maf 0.1 --max-missing 1 --vcf $DATADIRECTORY/individuals.vcf_DP10.vcf --recode --out $DATADIRECTORY/individuals.vcf_DP10_maf0.1_miss1.vcf
