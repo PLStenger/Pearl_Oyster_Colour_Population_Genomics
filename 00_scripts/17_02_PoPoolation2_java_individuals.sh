79e5629435aa7b16ad5e89c6cd568146d4dc77c6
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-10-24 08:49:11 -1000

    Update and rename 17_02_PoPoolation2_filter_individuals.sh to 17_02_PoPoolation2_java_individuals.sh

---
+++ 00_scripts/17_02_PoPoolation2_java_individuals.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#PBS -v DOCKER_IMAGE=“docker.ifremer.fr/bioinfo/popoolation2:1201”
+#PBS -q omp
+#PBS -l walltime=48:00:00
+#PBS -l mem=70g
+#PBS -l ncpus=10
+
+BAM=/home1/scratch/plstenge/freebayes	
+
+cd $BAM
+
+$POPOO_HOME
+java -ea -Xmx7g -jar <popoolation2-path>/mpileup2sync.jar --input all_12_bam.mpileup --output all_12_bam_mpileup_java.sync --fastq-type sanger --min-qual 20 --threads 8
