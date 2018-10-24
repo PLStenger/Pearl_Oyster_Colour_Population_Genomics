d44d880c8c4389b0cf3d499869df601a7f921046
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-10-24 13:01:49 -1000

    Update 17_02_PoPoolation2_java_individuals.sh

--- 00_scripts/17_02_PoPoolation2_java_individuals.sh
+++ 00_scripts/17_02_PoPoolation2_java_individuals.sh
@@ -9,4 +9,15 @@ BAM=/home1/scratch/plstenge/freebayes
 
 cd $BAM
 
-java -ea -Xmx7g -jar $POPOO_HOME/mpileup2sync.jar --input all_12_bam.mpileup --output all_12_bam_mpileup_java.sync --fastq-type sanger --min-qual 20 --threads 8 >& $HOME/popoo_sync.log
+# java -ea -Xmx7g -jar $POPOO_HOME/mpileup2sync.jar --input all_12_bam.mpileup --output all_12_bam_mpileup_java.sync --fastq-type sanger --min-qual 20 --threads 8 >& $HOME/popoo_sync.log
+
+perl  $POPOO_HOME/mpileup2sync.pl --fastq-type sanger --min-qual 20 --input all_12_bam.mpileup --output all_12_bam.mpileup.sync >& $HOME/popoo_sync.log
+
+
+# Vai:
+#perl $POPOO_HOME/fisher-test.pl --input $DATADIR/PoolFemalesVSAllMales_trimmed_genome_java.sync \
+#                       --output $DATADIR/PoolFemalesVSAllMales_trimmed_genome_minDP7.fet \
+#                       --suppress-noninformative \
+#                       --min-coverage 7 \
+#                       2> "$LOG_FOLDER"/log.FET_PoolFemalesVSAllMales_trimmed_genome_minDP7_popoodocker."$TIMESTAMP"
+
