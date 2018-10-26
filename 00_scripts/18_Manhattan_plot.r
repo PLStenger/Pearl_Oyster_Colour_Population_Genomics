b8641e48550a359bb9f9b25fb8b4737f536144c0
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-10-26 08:14:50 -1000

    Create 18_Manhattan_plot.r

---
+++ 00_scripts/18_Manhattan_plot.r
@@ -0,0 +1,14 @@
+#!/usr/bin/env Rscript
+
+setwd("/home1/scratch/creisser/PL_BAM_MD/")
+
+library(qqman)
+
+
+dat <- read.table("individuals.vcf_DP10_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.copy.modif_chi2_v3", header=T)
+dat2 <- data.frame(dat$CHROM, dat$POS, dat$POS, dat$P)
+colnames(dat2) <- c("SNP", "CHR", "BP", "P")
+
+
+
+manhattan(dat2)
