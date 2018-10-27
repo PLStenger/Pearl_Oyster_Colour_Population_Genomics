a3013ebac108a13fc735658a70084bd7983b5b96
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-10-26 14:16:18 -1000

    Update 18_Manhattan_plot.R

--- 00_scripts/18_Manhattan_plot.R
+++ 00_scripts/18_Manhattan_plot.R
@@ -24,7 +24,6 @@ ggplot(don, aes(x=CHR, y=-log10(P))) +
     
     # Show all points
     geom_point( aes(color=as.factor(CHR)), alpha=0.8, size=1.3) +
-    scale_color_manual(values = rep(c("grey", "skyblue"), 22 )) +
     
   
     # Custom the theme:
