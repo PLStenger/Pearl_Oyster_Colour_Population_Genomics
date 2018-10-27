dee70c0a26964d9d30dbbb3edd3f3a54e7a5cdd2
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-10-26 18:56:27 -1000

    Create 16_03_freebayes_paralell_merged_vcftools.sh

---
+++ 00_scripts/16_03_freebayes_paralell_merged_vcftools.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#PBS -q mpi
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=28:mem=115g
+
+# Data from 12_01_freebayes_pool_no_genotypes.sh
+
+DATADIRECTORY=/home1/scratch/plstenge/merged_bam
+VCFTOOLSENV=". /appli/bioinfo/vcftools/0.1.14/env.sh"
+
+$VCFTOOLSENV
+cd $DATADIRECTORY
+
+# --max-missing 1 = On garde tout 
+
+vcftools --maf 0.1 --max-missing 1 --vcf $DATADIRECTORY/merged_bam_ryg_genotypes_paralell_DP50.vcf --recode --out $DATADIRECTORY/merged_bam_ryg_genotypes_paralell_DP50_maf0.1_miss1.vcf
