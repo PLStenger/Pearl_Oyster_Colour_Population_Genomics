df49b822ef46512ec638140f1be7c92607771097
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-11-05 08:57:11 -1000

    Update 18_Manhattan_plot.R

--- 00_scripts/18_Manhattan_plot.R
+++ 00_scripts/18_Manhattan_plot.R
@@ -1,12 +1,13 @@
 #!/usr/bin/env Rscript
 
-setwd("/home1/scratch/creisser/PL_BAM_MD/")
+setwd("/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/08_chi2")
 
 library(ggplot2)
 library(dplyr)
+library(grDevices)
 
 
-dat <- read.table("merged_bam_ryg_paralell_DP50_maf0.1_miss1_recode_bcfm2M2v.vcf.modif_chi2_v3_DP20_pool", skip=1)
+dat <- read.table("individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.copy.modif_chi2_v3", skip=1)
 colnames(dat) <- c("CHROM", "POS", "REF", "ALT", "P")
 dat2 <- data.frame(dat$CHROM, dat$POS, dat$POS, dat$P)
 colnames(dat2) <- c("SNP", "CHR", "BP", "P")
@@ -18,9 +19,9 @@ P = dat2$P
 SNP = dat2$SNP
 BP = dat2$BP
 
+png("individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.copy.modif_chi2_v3_ggplot2.png")
 
-
-ggplot(don, aes(x=CHR, y=-log10(P))) +
+myplot <-  ggplot(don, aes(x=CHR, y=-log10(P))) +
     
     # Show all points
     geom_point( aes(color=as.factor(CHR)), alpha=0.8, size=1.3) +
@@ -35,5 +36,6 @@ ggplot(don, aes(x=CHR, y=-log10(P))) +
       panel.grid.minor.x = element_blank()
     )
 
-
+print(myplot)
+dev.off()
 
