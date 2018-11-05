9b71593040ffd51718c25ef6b93e1389f583dd6c
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-11-05 10:03:54 -1000

    Update 18_Manhattan_plot.R

--- 00_scripts/18_Manhattan_plot.R
+++ 00_scripts/18_Manhattan_plot.R
@@ -6,7 +6,7 @@ library(ggplot2)
 library(dplyr)
 
 
-dat <- read.table("individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.copy.modif_chi2_v3", skip=1)
+dat <- read.table("individuals.vcf_DP10_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.copy.modif_chi2_v3", skip=1)
 colnames(dat) <- c("CHROM", "POS", "REF", "ALT", "P")
 dat2 <- data.frame(dat$CHROM, dat$POS, dat$POS, dat$P)
 colnames(dat2) <- c("SNP", "CHR", "BP", "P")
@@ -33,5 +33,35 @@ ggplot(don, aes(x=CHR, y=-log10(P))) +
       panel.grid.minor.x = element_blank()
     )
 
-ggsave("individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.copy.modif_chi2_v3_ggplot2.png")
+ggsave("individuals.vcf_DP10_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.copy.modif_chi2_v3_ggplot2.png")
+
+
+dat <- read.table("merged_bam_ryg_paralell_DP50_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.modif_chi2_v4", skip=1)
+colnames(dat) <- c("CHROM", "POS", "REF", "ALT", "P")
+dat2 <- data.frame(dat$CHROM, dat$POS, dat$POS, dat$P)
+colnames(dat2) <- c("SNP", "CHR", "BP", "P")
+
+don <- dat2
+
+CHR = dat2$CHR
+P = dat2$P
+SNP = dat2$SNP
+BP = dat2$BP
+
+ggplot(don, aes(x=CHR, y=-log10(P))) +
+    
+    # Show all points
+    geom_point( aes(color=as.factor(CHR)), alpha=0.8, size=1.3) +
+    
+  
+    # Custom the theme:
+    theme_bw() +
+    theme( 
+      legend.position="none",
+      panel.border = element_blank(),
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank()
+    )
+
+ggsave("merged_bam_ryg_paralell_DP50_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.modif_chi2_v4_ggplot2.png")
 
