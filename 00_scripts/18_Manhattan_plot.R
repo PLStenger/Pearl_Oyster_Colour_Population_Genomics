a2ed9a39e0210e0fa9e72cc2b8ee95036c204644
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-11-05 09:04:10 -1000

    Update 18_Manhattan_plot.R

--- 00_scripts/18_Manhattan_plot.R
+++ 00_scripts/18_Manhattan_plot.R
@@ -4,7 +4,6 @@ setwd("/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/08_chi2"
 
 library(ggplot2)
 library(dplyr)
-library(grDevices)
 
 
 dat <- read.table("individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.copy.modif_chi2_v3", skip=1)
@@ -19,9 +18,7 @@ P = dat2$P
 SNP = dat2$SNP
 BP = dat2$BP
 
-png("individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.copy.modif_chi2_v3_ggplot2.png")
-
-myplot <-  ggplot(don, aes(x=CHR, y=-log10(P))) +
+ggplot(don, aes(x=CHR, y=-log10(P))) +
     
     # Show all points
     geom_point( aes(color=as.factor(CHR)), alpha=0.8, size=1.3) +
@@ -36,6 +33,5 @@ myplot <-  ggplot(don, aes(x=CHR, y=-log10(P))) +
       panel.grid.minor.x = element_blank()
     )
 
-print(myplot)
-dev.off()
+ggsave("individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.copy.modif_chi2_v3_ggplot2.png")
 
