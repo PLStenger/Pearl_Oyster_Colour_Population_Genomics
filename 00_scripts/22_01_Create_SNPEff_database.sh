3db7002305cb01fc1d5b923d8adda12ffe3e9c57
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-11-08 13:56:44 -1000

    Create 22_01_Create_SNPEff_database.sh

---
+++ 00_scripts/22_01_Create_SNPEff_database.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#PBS -q mpi
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=28:mem=115g
+
+SNPEFF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/95_software #/Path/to/snpEff/Folder
+GFF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds.gff3 # 01_info_files/your_genome.gff3
+FASTA=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds.fasta # 01_info_files/your_genome.fasta
+DBNAME=your_genome
+WORKDIR=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/09_snpEff #/your/path/to/WGS_pipeline
+
+cd $SNPEFF
+
+#create necessary directories for analysis in the SNPeff program folder: a directory "data" in wich two other directories is created:
+mkdir -p data
+mkdir -p data/$DBNAME
+mkdir -p data/genomes
+
+
+# Copy files where they need to be. GFF in the $DBNAME directory, fasta in the "genomes" directory
+cp $WORKDIR/$GFF ./data/$DBNAME/genes.gff
+cp $WORKDIR/01_info_files/$DBNAME.fasta ./data/genomes/$DBNAME.fa
+
+
+#Modify the config file in order to add the new genome
+echo "# Genome of your species, your_genome.fasta" >> snpEff.config
+echo "$DBNAME.genome : $DBNAME" >> snpEff.config
+
+java -Xmx115G -jar $SNPEFF/snpEff.jar build -gff3 -v $DBNAME
