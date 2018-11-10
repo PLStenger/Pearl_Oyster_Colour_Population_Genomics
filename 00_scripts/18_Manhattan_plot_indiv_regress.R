4ef4890e81369360cee6564fc51d2e36751544d6
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-11-09 16:16:36 -1000

    Update 18_Manhattan_plot_indiv_regress.R

--- 00_scripts/18_Manhattan_plot_indiv_regress.R
+++ 00_scripts/18_Manhattan_plot_indiv_regress.R
@@ -33,7 +33,7 @@ ggplot(don, aes(x=CHR, y=-log10(P))) +
     # Show all points
     geom_point( aes(color=as.factor(CHR)), alpha=0.8, size=1.3) +
     
-    ggtitle("individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.copy.logitR_ORIGIN) +
+    ggtitle("individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.copy.logitR_ORIGIN") +
     
   
     # Custom the theme:
@@ -64,7 +64,7 @@ ggplot(don, aes(x=CHR, y=-log10(P))) +
     # Show all points
     geom_point( aes(color=as.factor(CHR)), alpha=0.8, size=1.3) +
     
-      ggtitle("individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.copy.logitR_COLOR) +
+      ggtitle("individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.copy.logitR_COLOR") +
 
     # Custom the theme:
     theme_bw() +
@@ -95,7 +95,7 @@ ggplot(don, aes(x=CHR, y=-log10(P))) +
     # Show all points
     geom_point( aes(color=as.factor(CHR)), alpha=0.8, size=1.3) +
     
-      ggtitle("individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.copy.logitR_NUMBER_OF_INDIVIDUALS) +
+      ggtitle("individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf.copy.logitR_NUMBER_OF_INDIVIDUALS") +
 
     # Custom the theme:
     theme_bw() +
