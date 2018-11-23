b26b6c61c78d3bcaa64462103bebf1023206cfd1
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-11-23 10:37:16 -1000

    Update 17_01_Chi2_ind.sh

--- 00_scripts/17_01_Chi2_ind.sh
+++ 00_scripts/17_01_Chi2_ind.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#PBS parameters
+#PBS -q mpi
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=28:mem=115g
 
 
 WORKING_DIRECTORY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_vcf_files
