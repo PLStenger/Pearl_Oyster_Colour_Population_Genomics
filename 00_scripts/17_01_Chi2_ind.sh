bb49baba186a89dff598f130a57efaceecb590ce
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-11-23 13:19:21 -1000

    Update 17_01_Chi2_ind.sh

--- 00_scripts/17_01_Chi2_ind.sh
+++ 00_scripts/17_01_Chi2_ind.sh
@@ -3,6 +3,8 @@
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=28:mem=115g
 
+module load anaconda-py2.7/4.3.13
+
 
 WORKING_DIRECTORY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_vcf_files
 PYTHON_SCRIPT=get_AF_from_vcf_v4.py
@@ -12,9 +14,9 @@ FINAL_OUTPUT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/08
 LOG=${WORKING_DIRECTORY}/python.log
 
 #Load conda env
-export PATH=$PATH:/appli/anaconda/2.7/bin
+# export PATH=$PATH:/appli/anaconda/2.7/bin
 
-source activate $DATAWORK/96_env_conda/python_env
+# source activate $DATAWORK/96_env_conda/python_env
 
 #module load anaconda-py2.7/4.3.13
 
