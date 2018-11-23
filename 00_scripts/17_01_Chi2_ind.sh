8994ae8984f791d9c290f8f5b0d90e8e4ff69419
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-11-23 10:36:44 -1000

    Create 17_01_Chi2_ind.sh

---
+++ 00_scripts/17_01_Chi2_ind.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#PBS parameters
+
+
+WORKING_DIRECTORY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_vcf_files
+PYTHON_SCRIPT=get_AF_from_vcf_v4.py
+VCF_FILE=individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf
+BASENAME_VCF=${ORIGINAL_VCF_FILE##*/}
+FINAL_OUTPUT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/08_chi2/${BASENAME_VCF}._chi2
+LOG=${WORKING_DIRECTORY}/python.log
+
+#Load conda env
+export PATH=$PATH:/appli/anaconda/2.7/bin
+
+source activate python_env
+
+cd ${WORKING_DIRECTORY} 
+
+python ${PYTHON_SCRIPT} ${VCF_FILE} -t 16 -o ${FINAL_OUTPUT} >& ${LOG} 2>&1
