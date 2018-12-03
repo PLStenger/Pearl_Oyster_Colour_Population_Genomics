5474cda1327bb3385bbbeaffdb3d4b39aaa45fd3
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-12-03 08:59:06 -1000

    Update 17_01_Chi2_ind.sh

--- 00_scripts/17_01_Chi2_ind.sh
+++ 00_scripts/17_01_Chi2_ind.sh
@@ -5,7 +5,7 @@
 #PBS -l mem=115g
 
 WORKING_DIRECTORY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_vcf_files
-PYTHON_SCRIPT=17_01_Chi2_ind.py
+PYTHON_SCRIPT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/17_01_Chi2_ind.py
 VCF_FILE=individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf
 BASENAME_VCF=${ORIGINAL_VCF_FILE##*/}
 FINAL_OUTPUT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/08_chi2/${BASENAME_VCF}._chi2
