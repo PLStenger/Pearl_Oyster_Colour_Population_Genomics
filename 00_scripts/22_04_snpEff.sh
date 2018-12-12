5d8e6208462ea5ffd520a3218d2f60e2450c72fe
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2018-12-12 11:31:36 -1000

    Update 22_04_snpEff.sh

--- 00_scripts/22_04_snpEff.sh
+++ 00_scripts/22_04_snpEff.sh
@@ -3,7 +3,7 @@
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=28:mem=115g
 
-DATADIRECTORY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/97_tests
+DATADIRECTORY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/09_snpEff
 SNPEFFENV=". /appli/bioinfo/snpeff/latest/env.sh"
 
 # il faut d'abord mettre sur le fichier config la configuration du génome
@@ -11,12 +11,13 @@ SNPEFFENV=". /appli/bioinfo/snpeff/latest/env.sh"
 
 SNPEFF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/95_software/snpEff #/Path/to/snpEff/Folder
 DBNAME=sspace.final.scaffolds.fasta
-VCF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/97_tests/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v_tests_snpF_input_12355.vcf
+VCF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/09_snpEff/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v_noComplex_snpEff_input_RvsJ.vcf
 WORKDIR=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/09_snpEff
+
 
 
 cd $SNPEFF
 
-java -Xmx115G -jar $SNPEFF/snpEff.jar $DBNAME $VCF > $WORKDIR/$OUTDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf &&
-mv snpEff_summary.html $WORKDIR/$OUTDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v_report.html
-mv snpEff_genes.txt $WORKDIR/$OUTDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v_genes_summary.txt
+java -Xmx115G -jar $SNPEFF/snpEff.jar $DBNAME $VCF > $WORKDIR/$OUTDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v_noComplex_snpEff_input_RvsJ_SNPEff.vcf &&
+mv snpEff_summary.html $WORKDIR/$OUTDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v_noComplex_snpEff_input_RvsJ_SNPEff.vcf_report.html
+mv snpEff_genes.txt $WORKDIR/$OUTDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v_noComplex_snpEff_input_RvsJ_SNPEff.vcf_genes_summary.txt
