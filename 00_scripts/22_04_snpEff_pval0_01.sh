cad3b75d7b088fce355ddecd3274d941bb153a56
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-02-13 18:03:00 +0100

    Create 22_04_snpEff_pval0_01.sh

---
+++ 00_scripts/22_04_snpEff_pval0_01.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#PBS -q mpi
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=28:mem=115g
+
+DATADIRECTORY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/09_snpEff
+SNPEFFENV=". /appli/bioinfo/snpeff/latest/env.sh"
+
+# il faut d'abord mettre sur le fichier config la configuration du génome
+
+
+SNPEFF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/95_software/snpEff #/Path/to/snpEff/Folder
+DBNAME=sspace.final.scaffolds.fasta
+VCF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_01_vcf_files_modified/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v_noComplex_snpEff_input_VvsR_Pvalue0.01.vcf
+WORKDIR=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/09_snpEff
+
+
+
+cd $SNPEFF
+
+java -Xmx115G -jar $SNPEFF/snpEff.jar $DBNAME $VCF > $WORKDIR/$OUTDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v_noComplex_snpEff_input_VvsR_Pvalue0.01_SNPEff.vcf &&
+mv snpEff_summary.html $WORKDIR/$OUTDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v_noComplex_snpEff_input_VvsR_Pvalue0.01_SNPEff.vcf_report.html
+mv snpEff_genes.txt $WORKDIR/$OUTDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v_noComplex_snpEff_input_VvsR_Pvalue0.01_SNPEff.vcf_genes_summary.txt
