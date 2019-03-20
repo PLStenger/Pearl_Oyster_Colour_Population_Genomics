8fb2fb69e0caead6210681103b9fadd45ebe8928
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-03-20 21:10:24 +0100

    Update 16_08_deleted_header_singleID.sh

--- 00_scripts/16_08_deleted_header_singleID.sh
+++ 00_scripts/16_08_deleted_header_singleID.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#PBS -q mpi
-#PBS -l walltime=24:00:00
-#PBS -l select=1:ncpus=28:mem=115g
+#PBS -q sequentiel
+#PBS -l walltime=720:00:00
+#PBS -l mem=1024g
 
 
 # Ce script permet de pouvoir modifier et formater le VCF pour les analyses statistiques suivantes, notament d'enlever le header et d'obtenir un noms de SNP unique en fusion le nom du scaffold avec la position
@@ -15,13 +15,11 @@ cd $INDIR
 
 # Enlever le header (comprenant les "#")
 
-grep -v "^#" $INDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex.vcf
- > $INDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex_no_header.vcf
+grep -v "^#" $INDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex.vcf > $INDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex_no_header.vcf
 
 # Et pour stocker le header dans un autre fichier:
 
-grep "^#" $INDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex.vcf
- > $INDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex_header.vcf
+grep "^#" $INDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex.vcf > $INDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex_header.vcf
 
 # On va maintenant merged les noms de scaffold et la position des SNP afin d'avoir un nom unique pour chaque SNP
 
