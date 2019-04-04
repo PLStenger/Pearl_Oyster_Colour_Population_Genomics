6f4b38005ec8fcce05b13fc39f640ad573d54094
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-04-04 15:13:25 +0200

    Update 16_08_deleted_header_singleID.sh

--- 00_scripts/16_08_deleted_header_singleID.sh
+++ 00_scripts/16_08_deleted_header_singleID.sh
@@ -64,3 +64,36 @@ rm $INDIR/only_end2DC.txt
 rm $INDIR/only_end3DC.txt 
 rm $INDIR/only_end4DC.txt
 rm $INDIR/only_end5DC.txt
+
+
+
+# In R version (exept the beginning)
+
+## In order to deal with some problems after Freebayes (less columns than usually / fusion of cells) and create a unique SNP name ("Scaffold name" + position)
+#system('awk \'{$9=$10=$11=$12=$13=$14=$15=$16=$17=$18=$19=$20=$21=""; print $1"_"$2"\t"$0}\' individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex_no_header.txt > OK.txt')
+#system('awk \'{gsub("GT:DP:AD:RO:QR:AO:QA:GL", "", $0); print $0}\' OK.txt > OK2.txt')
+#system('awk \'{gsub("GT:DP:AD:RO:QR:AO:QA:GL", "\t.", $0); print $0}\' OK.txt > OK2.txt')
+#system('awk \'{gsub(" \t.", " .", $0); print $0}\' OK2.txt > OK2a.txt')
+#system('awk \'{$10=""; print $0}\' OK2a.txt > OK2b.txt')
+#system('awk \'{$3=$4=$4=$6=$7=$8=""; print $1"_"$2"\t"$0}\' individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex_no_header.txt > OK_end1.txt')
+#system('awk \'{$2=$3=$4=""; print $0}\' OK_end1.txt > OK_end1a.txt')
+#
+#
+#system('awk \'{gsub("GT:DP:AD:RO:QR:AO:QA:GL", "", $0); print $0}\' OK_end1a.txt > OK_end3.txt')
+#
+#
+## On the only end file, remove all tabulation
+#system('sed -e \'s/\t//g\' OK_end3.txt > only_end2DC.txt')
+#
+## remove the 0/0:, 0/1: and 1/1: in three step
+#system('cat only_end2DC.txt | awk \'{gsub("0/1:", "\t", $0); print $0}\' > only_end3DC.txt')
+#system('cat only_end3DC.txt | awk \'{gsub("0/0:", "\t", $0); print $0}\' > only_end4DC.txt')
+#system('cat only_end4DC.txt | awk \'{gsub("1/1:", "\t", $0); print $0}\' > only_end5DC.txt')
+#
+#
+## Merge the start and the end together
+#system('join -a1 OK2b.txt only_end5DC.txt > individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex_no_header_inputRL_PL.txt')
+#
+## Remove temp files
+#file.remove("OK_end1.txt", "OK_end1a.txt", "OK_end3.txt", "OK.txt", "OK2.txt", "OK2a.txt", "OK2b.txt", "only_end2DC.txt", "only_end3DC.txt", "only_end4DC.txt", "only_end5DC.txt")
+#
