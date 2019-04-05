8492ee692cc4c7c0177adb4d6d6128d796c81907
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-04-05 14:28:31 +0200

    Add files via upload

---
+++ 00_scripts/17_00_09_glm_Tuckey.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#PBS -q sequentiel
+#PBS -l walltime=150:00:00
+#PBS -l mem=500g
+
+DATAWORK=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_01_vcf_files_modified
+
+cd $DATAWORK
+
+#Chargement de conda
+export PATH=$PATH:/appli/anaconda/2.7/bin
+
+#Chargement de votre environnement
+source activate /home1/datawork/plstenge/96_env_conda/glm
+
+SCRIPT_R=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/17_00_09_split_glm_Tuckey.R
+
+Rscript --vanilla $SCRIPT_R >& ${SCRATCH}/fichier2sortie_09_split_glm_Tuckey.out
