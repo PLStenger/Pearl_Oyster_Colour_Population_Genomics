87fe9a512ce2a9b6c0b44087b56c1dd306a1080a
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-04-14 19:54:37 +0200

    Create 17_00_03_glm_Tuckey_with_iteration_00.sh

---
+++ 00_scripts/17_00_03_glm_Tuckey_with_iteration_00.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#PBS -q omp
+#PBS -l walltime=180:00:00
+#PBS -l mem=1024g
+#PBS -l ncpus=6
+
+DATAWORK=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_01_vcf_files_modified
+
+cd $DATAWORK
+
+#Chargement de conda
+export PATH=$PATH:/appli/anaconda/2.7/bin
+
+#Chargement de votre environnement
+source activate /home1/datawork/plstenge/96_env_conda/glm
+
+SCRIPT_R=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/17_00_03_glm_Tuckey_with_iteration_00.R
+
+Rscript --vanilla $SCRIPT_R >& ${SCRATCH}/17_00_03_glm_Tuckey_with_iteration_00.out
