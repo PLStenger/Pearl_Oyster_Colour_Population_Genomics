4ae8df653c955c52bcca3e4e22d4b91ee81a3db3
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-04-15 17:04:30 +0200

    Update and rename 17_00_03_glm_Tuckey_with_iteration_all_not_ok01.sh to 17_00_06_glm_Tuckey_with_iteration_all_not_ok01.sh

--- 00_scripts/17_00_06_glm_Tuckey_with_iteration_all_not_ok01.sh
+++ 00_scripts/17_00_06_glm_Tuckey_with_iteration_all_not_ok01.sh
@@ -14,6 +14,6 @@ export PATH=$PATH:/appli/anaconda/2.7/bin
 #Chargement de votre environnement
 source activate /home1/datawork/plstenge/96_env_conda/glm
 
-SCRIPT_R=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/17_00_03_glm_Tuckey_with_iteration_all_not_ok01.R
+SCRIPT_R=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/17_00_06_glm_Tuckey_with_iteration_all_not_ok01.R
 
-Rscript --vanilla $SCRIPT_R >& ${SCRATCH}/17_00_03_glm_Tuckey_with_iteration_all_not_ok01.out
+Rscript --vanilla $SCRIPT_R >& ${SCRATCH}/17_00_06_glm_Tuckey_with_iteration_all_not_ok01.out
