bb0ceacffbf52e32ce63e9cf1a28775eac360e8f
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-04-15 17:01:44 +0200

    Update 17_00_04_clean_the_output_results_bis.sh

--- 00_scripts/17_00_04_clean_the_output_results_bis.sh
+++ 00_scripts/17_00_04_clean_the_output_results_bis.sh
@@ -315,5 +315,32 @@ awk '$1=="10"' input_glm_split_with_iteration_results_10_without_dbsp_no_rt_scaf
 awk '$1!="10"' input_glm_split_with_iteration_results_10_without_dbsp_no_rt_scaf_dbsp_nb_line_merged.txt > input_glm_split_with_iteration_results_10_not_ok.txt
 
 
+# Clean the output results from the glm
+# Delete the double spaces
+sed -e 's/\ \ //g'  input_glm_split_with_iteration_results_29.txt >  input_glm_split_with_iteration_results_29_without_dbsp.txt
+
+# Delete return to the line
+tr -d '\n' <  input_glm_split_with_iteration_results_29_without_dbsp.txt >  input_glm_split_with_iteration_results_29_without_dbsp_no_rt.txt
+
+# Create return to the line if "scaffold" pattern
+sed $'s/scaffold/\\\nscaffold/g'  input_glm_split_with_iteration_results_29_without_dbsp_no_rt.txt >  input_glm_split_with_iteration_results_29_without_dbsp_no_rt_scaf.txt
+sed -e 's/\ \ /\ /g'  input_glm_split_with_iteration_results_29_without_dbsp_no_rt_scaf.txt >  input_glm_split_with_iteration_results_29_without_dbsp_no_rt_scaf_dbsp.txt
+
+# Si pas 10 block, met la ligne dans un autre fichier:
+
+# Compte le nombre de "mots" par ligne
+awk '$0="line"NR": "NF'  input_glm_split_with_iteration_results_29_without_dbsp_no_rt_scaf_dbsp.txt >  input_glm_split_with_iteration_results_29_without_dbsp_no_rt_scaf_dbsp_line_nb.txt
+
+# Enlever "lineX:" pour n'avoir que la valeur du nombre de mots
+awk '{$1=""; print $2}'  input_glm_split_with_iteration_results_29_without_dbsp_no_rt_scaf_dbsp_line_nb.txt >  input_glm_split_with_iteration_results_29_without_dbsp_no_rt_scaf_dbsp_line_nb_clean.txt
+
+# Merge the 2 files (nb of lines with the output)
+paste  input_glm_split_with_iteration_results_29_without_dbsp_no_rt_scaf_dbsp_line_nb_clean.txt  input_glm_split_with_iteration_results_29_without_dbsp_no_rt_scaf_dbsp.txt >  input_glm_split_with_iteration_results_29_without_dbsp_no_rt_scaf_dbsp_nb_line_merged.txt
+
+# If 10 put in output_ok.txt
+awk '$1=="10"'  input_glm_split_with_iteration_results_29_without_dbsp_no_rt_scaf_dbsp_nb_line_merged.txt >  input_glm_split_with_iteration_results_29_ok.txt
+
+# If not 10 put in output_not_ok.txt
+awk '$1!="10"'  input_glm_split_with_iteration_results_29_without_dbsp_no_rt_scaf_dbsp_nb_line_merged.txt >  input_glm_split_with_iteration_results_29_not_ok.txt
 
 
