22764e383f313d34e09f46df6be00745511efc27
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-04-16 14:27:27 +0200

    Update 17_00_02_preparation_glm_Tuckey_with_iteration.R

--- 00_scripts/17_00_02_preparation_glm_Tuckey_with_iteration.R
+++ 00_scripts/17_00_02_preparation_glm_Tuckey_with_iteration.R
@@ -7,4 +7,4 @@ df <- read.table("df.txt", header=T)
 df2 <- df[order(df$SNP),]
 names(df2) <- NULL
 write.table(df2, file ="df2.txt")
-system('split -d -l 26800000 df2.txt input_glm_split_with_iteration_')
+system('split -d -l 13400000 df2.txt input_glm_split_with_iteration_')
