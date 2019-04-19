8b4981d97ce29e139e61b3aeb1607e83b86b3d9e
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-04-19 16:58:34 +0200

    Create 17_00_05_03_split_input_for_problematics_scaffolds.R

---
+++ 00_scripts/17_00_05_03_split_input_for_problematics_scaffolds.R
@@ -0,0 +1,5 @@
+#!/usr/bin/env Rscript
+
+setwd("/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_01_vcf_files_modified/")
+
+system('split -d -l 13400000 for_rerun.txt input_glm_split_with_iteration_for_rerun_')
