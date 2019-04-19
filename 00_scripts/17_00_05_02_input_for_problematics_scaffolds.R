cb452181a32b0b34e358cd5d9d3a0949dba5a324
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-04-19 11:48:10 +0200

    Update 17_00_05_02_input_for_problematics_scaffolds.R

--- 00_scripts/17_00_05_02_input_for_problematics_scaffolds.R
+++ 00_scripts/17_00_05_02_input_for_problematics_scaffolds.R
@@ -2,7 +2,7 @@
 
 setwd("/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_01_vcf_files_modified/")
 
-all <- read.table("input_glm_split_with_iteration_all.txt", header=F)
+all <- read.table("input_glm_split_with_iteration_all", header=F)
 
 ok <- read.table("input_glm_split_with_iteration_results_00_01_02_03_04_05_06_07_08_09_10_11_12_13_14_15_16_17_18_ok.txt", header=F)
 
