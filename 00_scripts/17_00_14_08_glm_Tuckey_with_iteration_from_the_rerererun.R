170bfc12a739205522f042aff0868964a1418c56
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-04-24 18:04:09 +0200

    Add files via upload

---
+++ 00_scripts/17_00_14_08_glm_Tuckey_with_iteration_from_the_rerererun.R
@@ -0,0 +1,38 @@
+#!/usr/bin/env Rscript
+
+setwd("/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_01_vcf_files_modified/")
+
+library(readr)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(multcomp)
+library(foreach)
+library(doParallel)
+
+df <- read.table("input_glm_split_with_iteration_for_rerererun_08", header=F)
+colnames(df) <- c("nothing", "SNP", "Site", "Color", "Frequence")
+head(df)
+
+registerDoParallel()
+
+  foreach(i=1:length(unique(df$SNP)), .combine=c) %dopar% {
+    sink("input_glm_split_with_iteration_results_from_rerererun_08.txt", append=TRUE)
+    SNP_name <- as.character(unique(df$SNP)[i])
+    ok <- filter(df, df$SNP  == unique(df$SNP)[i])
+    mod <- glm(Frequence ~ Color + Site, data = ok)
+    K1 <- glht(mod, mcp(Color = "Tukey"))$linfct
+    K2 <- glht(mod, mcp(Site = "Tukey"))$linfct
+    pvaleur <- summary(glht(mod, linfct = rbind(K1, K2)))$test$pvalues[1:9]
+    pvaleur <- c(pvaleur)
+    test <- c(SNP_name, pvaleur)
+    test2 <- data.frame(as.list(test))
+    names(test2) <- NULL
+    print(test2, row.names=FALSE)
+    sink()
+  }
+
+
+
+
+registerDoSEQ()
