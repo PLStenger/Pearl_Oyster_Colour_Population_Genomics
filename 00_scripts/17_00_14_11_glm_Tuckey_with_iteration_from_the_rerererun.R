cf2e28332945170d8b2e4aa20dcde2846692f001
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-04-24 18:12:26 +0200

    Update 17_00_14_11_glm_Tuckey_with_iteration_from_the_rerererun.R

--- 00_scripts/17_00_14_11_glm_Tuckey_with_iteration_from_the_rerererun.R
+++ 00_scripts/17_00_14_11_glm_Tuckey_with_iteration_from_the_rerererun.R
@@ -11,7 +11,7 @@ library(foreach)
 library(doParallel)
 
 df <- read.table("input_glm_split_with_iteration_for_rerererun_11", header=F)
-colnames(df) <- c("nothing", "SNP", "Site", "Color", "Frequence")
+colnames(df) <- c("nb", "nothing", "SNP", "Site", "Color", "Frequence")
 head(df)
 
 registerDoParallel()
