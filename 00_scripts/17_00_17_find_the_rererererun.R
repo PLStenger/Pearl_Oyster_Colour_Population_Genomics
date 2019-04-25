6a7837cfa285285d9fc4a9aed54cdbaf2eba7624
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-04-25 10:39:50 +0200

    Update 17_00_17_find_the_rererererun.R

--- 00_scripts/17_00_17_find_the_rererererun.R
+++ 00_scripts/17_00_17_find_the_rererererun.R
@@ -8,6 +8,6 @@ head(for_rerererun)
 ok <- read.table("input_glm_split_with_iteration_results_from_rerererun_00_01_02_03_04_05_06_07_08_09_10_11_ok.txt", header=F)
 head(ok)
 
-for_rererererun <- for_rerererun[!for_rerererun$V2 %in% ok$V2, , drop = FALSE]
+for_rererererun <- for_rerererun[!for_rerererun$V3 %in% ok$V2, , drop = FALSE]
 
 write.table(for_rererererun, "for_rererererun.txt")
