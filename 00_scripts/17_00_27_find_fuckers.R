418828b2649338a919cfec6975ba5ff64bd1f9d5
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-04-26 16:17:35 +0200

    Create 17_00_27_find_fuckers.R

---
+++ 00_scripts/17_00_27_find_fuckers.R
@@ -0,0 +1,13 @@
+#!/usr/bin/env Rscript
+
+setwd("/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_01_vcf_files_modified")
+
+for_rerun <- read.table("input_glm_split_with_iteration_all", header=F)
+head(for_rerun)
+
+ok <- read.table("all_results_ok_from_glm_with_iteration_unique_final.txt", header=F)
+head(ok)
+
+for_rererun <- for_rerun[!for_rerun$V2 %in% ok$V2, , drop = FALSE]
+
+write.table(for_rererun, "les_fucking_derniers_qui_ne_passent_pas_et_qui_casse_les.txt")
