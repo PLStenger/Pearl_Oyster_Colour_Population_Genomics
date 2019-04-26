aca0578ff8bc0b69f107a7ca05d7a05a4b6f83a6
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-04-26 16:17:33 +0200

    Create 17_00_27_find_fuckers.sh

---
+++ 00_scripts/17_00_27_find_fuckers.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#PBS -q mpi
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=28:mem=115g
+
+DATAWORK=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_01_vcf_files_modified
+
+cd $DATAWORK
+
+#Chargement de conda
+export PATH=$PATH:/appli/anaconda/2.7/bin
+
+#Chargement de votre environnement
+source activate /home1/datawork/plstenge/96_env_conda/glm
+
+SCRIPT_R=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/17_00_27_find_fuckers.R
+
+Rscript --vanilla $SCRIPT_R >& ${SCRATCH}/17_00_27_find_fuckers.out
