f679b03f24bc22ef6629ad0cd06ea2b831c52070
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-04-29 10:32:49 +0200

    Update 17_00_27_find_fuckers.R

--- 00_scripts/17_00_27_find_fuckers.R
+++ 00_scripts/17_00_27_find_fuckers.R
@@ -8,6 +8,6 @@ head(for_rerun)
 ok <- read.table("all_results_ok_from_glm_with_iteration_unique_final.txt", header=T)
 head(ok)
 
-for_rererun <- for_rerun[!for_rerun$V2 %in% ok$V2, , drop = FALSE]
+for_rererun <- for_rerun[for_rerun$V2 %in% ok$V2, , drop = FALSE]
 
 write.table(for_rererun, "les_fucking_derniers_qui_ne_passent_pas_et_qui_casse_les.txt")
