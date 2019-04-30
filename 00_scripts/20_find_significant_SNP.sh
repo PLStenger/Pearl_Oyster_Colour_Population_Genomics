229e2f15fc51b43f0cea46ff63f9c93e524f0da5
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-04-30 16:43:52 +0200

    Create 20_find_significant_SNP.sh

---
+++ 00_scripts/20_find_significant_SNP.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#PBS -q mpi
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=28:mem=115g
+
+
+DATAWORK=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/08_glm_tuckey
+
+cd $DATAWORK
+
+#Chargement de conda
+export PATH=$PATH:/appli/anaconda/2.7/bin
+
+#Chargement de votre environnement
+source activate /home1/datawork/plstenge/96_env_conda/glm
+
+SCRIPT_R=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/20_find_significant_SNP.R
+
+Rscript --vanilla $SCRIPT_R >& ${SCRATCH}/20_find_significant_SNP.out
