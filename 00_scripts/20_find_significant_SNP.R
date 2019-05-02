b8446610dc1aa47c49983e7bd6bffd4fbc17f0b2
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-02 13:09:28 +0200

    Update 20_find_significant_SNP.R

--- 00_scripts/20_find_significant_SNP.R
+++ 00_scripts/20_find_significant_SNP.R
@@ -24,84 +24,84 @@ print("head(dat$RedVsGreen)")
 head(dat$RedVsGreen)
 print("head(RedVsGreen)")
 head(RedVsGreen)
-datRedVsGreen <- subset(dat, RedVsGreen<0.000001)
+datRedVsGreen <- subset(dat, dat$RedVsGreen<0.000001)
 print("head(datRedVsGreen)")
 head(datRedVsGreen)
 write.table(datRedVsGreen$SNP, "RedVsGreen_P_0_000001.txt")
-datRedVsGreen <- subset(dat, RedVsGreen<0.001)
+datRedVsGreen <- subset(dat, dat$RedVsGreen<0.001)
 write.table(datRedVsGreen$SNP, "RedVsGreen_P_0_001.txt")
-datRedVsGreen <- subset(dat, RedVsGreen<0.01)
+datRedVsGreen <- subset(dat, dat$RedVsGreen<0.01)
 write.table(datRedVsGreen$SNP, "RedVsGreen_P_0_01.txt")
 
 ######################################################################################################
 ## YellowVsGreen
-datYellowVsGreen <- subset(dat, YellowVsGreen<0.000001)
+datYellowVsGreen <- subset(dat, dat$YellowVsGreen<0.000001)
 write.table(datYellowVsGreen$SNP, "YellowVsGreen_P_0_000001.txt")
-datYellowVsGreen <- subset(dat, YellowVsGreen<0.001)
+datYellowVsGreen <- subset(dat, dat$YellowVsGreen<0.001)
 write.table(datYellowVsGreen$SNP, "YellowVsGreen_P_0_001.txt")
-datYellowVsGreen <- subset(dat, YellowVsGreen<0.01)
+datYellowVsGreen <- subset(dat, dat$YellowVsGreen<0.01)
 write.table(datYellowVsGreen$SNP, "YellowVsGreen_P_0_01.txt")
 
 ######################################################################################################
 ## YellowVsRed
-datYellowVsRed <- subset(dat, YellowVsRed<0.000001)
+datYellowVsRed <- subset(dat, dat$YellowVsRed<0.000001)
 write.table(datYellowVsRed$SNP, "YellowVsRed_P_0_000001.txt")
-datYellowVsRed <- subset(dat, YellowVsRed<0.001)
+datYellowVsRed <- subset(dat, dat$YellowVsRed<0.001)
 write.table(datYellowVsRed$SNP, "YellowVsRed_P_0_001.txt")
-datYellowVsRed <- subset(dat, YellowVsRed<0.01)
+datYellowVsRed <- subset(dat, dat$YellowVsRed<0.01)
 write.table(datYellowVsRed$SNP, "YellowVsRed_P_0_01.txt")
 
 
 ######################################################################################################
 ## HatcheryVsGambier
-datHatcheryVsGambier <- subset(dat, HatcheryVsGambier<0.000001)
+datHatcheryVsGambier <- subset(dat, dat$HatcheryVsGambier<0.000001)
 write.table(datHatcheryVsGambier$SNP, "HatcheryVsGambier_P_0_000001.txt")
-datHatcheryVsGambier <- subset(dat, HatcheryVsGambier<0.001)
+datHatcheryVsGambier <- subset(dat, dat$HatcheryVsGambier<0.001)
 write.table(datHatcheryVsGambier$SNP, "HatcheryVsGambier_P_0_001.txt")
-datHatcheryVsGambier <- subset(dat, HatcheryVsGambier<0.01)
+datHatcheryVsGambier <- subset(dat, dat$HatcheryVsGambier<0.01)
 write.table(datHatcheryVsGambier$SNP, "HatcheryVsGambier_P_0_01.txt")
 
 ######################################################################################################
 ## KatiuVsGambier
-datKatiuVsGambier <- subset(dat, KatiuVsGambier<0.000001)
+datKatiuVsGambier <- subset(dat, dat$KatiuVsGambier<0.000001)
 write.table(datKatiuVsGambier$SNP, "KatiuVsGambier_P_0_000001.txt")
-datKatiuVsGambier <- subset(dat, KatiuVsGambier<0.001)
+datKatiuVsGambier <- subset(dat, dat$KatiuVsGambier<0.001)
 write.table(datKatiuVsGambier$SNP, "KatiuVsGambier_P_0_001.txt")
-datKatiuVsGambier <- subset(dat, KatiuVsGambier<0.01)
+datKatiuVsGambier <- subset(dat, dat$KatiuVsGambier<0.01)
 write.table(datKatiuVsGambier$SNP, "KatiuVsGambier_P_0_01.txt")
 
 ######################################################################################################
 ## TakapotoVsGambier
-datTakapotoVsGambier <- subset(dat, TakapotoVsGambier<0.000001)
+datTakapotoVsGambier <- subset(dat, dat$TakapotoVsGambier<0.000001)
 write.table(datTakapotoVsGambier$SNP, "TakapotoVsGambier_P_0_000001.txt")
-datTakapotoVsGambier <- subset(dat, TakapotoVsGambier<0.001)
+datTakapotoVsGambier <- subset(dat, dat$TakapotoVsGambier<0.001)
 write.table(datTakapotoVsGambier$SNP, "TakapotoVsGambier_P_0_001.txt")
-datTakapotoVsGambier <- subset(dat, TakapotoVsGambier<0.01)
+datTakapotoVsGambier <- subset(dat, dat$TakapotoVsGambier<0.01)
 write.table(datTakapotoVsGambier$SNP, "TakapotoVsGambier_P_0_01.txt")
 
 ######################################################################################################
 ## KatiuVsHatchery
-datKatiuVsHatchery <- subset(dat, KatiuVsHatchery<0.000001)
+datKatiuVsHatchery <- subset(dat, dat$KatiuVsHatchery<0.000001)
 write.table(datKatiuVsHatchery$SNP, "KatiuVsHatchery_P_0_000001.txt")
-datKatiuVsHatchery <- subset(dat, KatiuVsHatchery<0.001)
+datKatiuVsHatchery <- subset(dat, dat$KatiuVsHatchery<0.001)
 write.table(datKatiuVsHatchery$SNP, "KatiuVsHatchery_P_0_001.txt")
-datKatiuVsHatchery <- subset(dat, KatiuVsHatchery<0.01)
+datKatiuVsHatchery <- subset(dat, dat$KatiuVsHatchery<0.01)
 write.table(datKatiuVsHatchery$SNP, "KatiuVsHatchery_P_0_01.txt")
 
 ######################################################################################################
 ## TakapotoVsHatchery
-datTakapotoVsHatchery <- subset(dat, TakapotoVsHatchery<0.000001)
+datTakapotoVsHatchery <- subset(dat, dat$TakapotoVsHatchery<0.000001)
 write.table(datTakapotoVsHatchery$SNP, "TakapotoVsHatchery_P_0_000001.txt")
-datTakapotoVsHatchery <- subset(dat, TakapotoVsHatchery<0.001)
+datTakapotoVsHatchery <- subset(dat, dat$TakapotoVsHatchery<0.001)
 write.table(datTakapotoVsHatchery$SNP, "TakapotoVsHatchery_P_0_001.txt")
-datTakapotoVsHatchery <- subset(dat, TakapotoVsHatchery<0.01)
+datTakapotoVsHatchery <- subset(dat, dat$TakapotoVsHatchery<0.01)
 write.table(datTakapotoVsHatchery$SNP, "TakapotoVsHatchery_P_0_01.txt")
 
 ######################################################################################################
 ## TakapotoVsKatiu
-datTakapotoVsKatiu <- subset(dat, TakapotoVsKatiu<0.000001)
+datTakapotoVsKatiu <- subset(dat, dat$TakapotoVsKatiu<0.000001)
 write.table(datTakapotoVsKatiu$SNP, "TakapotoVsKatiu_P_0_000001.txt")
-datTakapotoVsKatiu <- subset(dat, TakapotoVsKatiu<0.001)
+datTakapotoVsKatiu <- subset(dat, dat$TakapotoVsKatiu<0.001)
 write.table(datTakapotoVsKatiu$SNP, "TakapotoVsKatiu_P_0_001.txt")
-datTakapotoVsKatiu <- subset(dat, TakapotoVsKatiu<0.01)
+datTakapotoVsKatiu <- subset(dat, dat$TakapotoVsKatiu<0.01)
 write.table(datTakapotoVsKatiu$SNP, "TakapotoVsKatiu_P_0_01.txt")
