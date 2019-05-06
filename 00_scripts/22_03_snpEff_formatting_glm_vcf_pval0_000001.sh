c5c7f1188ef3df0b5f0a5cc32bbcbd67f326c97b
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-06 16:58:54 +0200

    Update 22_03_snpEff_formatting_glm_vcf_pval0_000001.sh

--- 00_scripts/22_03_snpEff_formatting_glm_vcf_pval0_000001.sh
+++ 00_scripts/22_03_snpEff_formatting_glm_vcf_pval0_000001.sh
@@ -20,7 +20,7 @@ cd $INDIR
 # on va chercher a avoir donc un vcf avec uniquement les snp qui nous interesse pour chaque comparaison
 awk '{print $4}' $INDIR/Total_HatcheryVsGambier_OK_dat_P_0_000001.txt > $INDIR/Total_HatcheryVsGambier_OK_dat_P_0_000001_SNP.txt
 
-awk 'NR == FNR {a[$1]=$1;next} ($NF in a) {print $0, a[$NF]}' $PATH_VCF/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex_no_header_inputRL_PL.txt $INDIR/Total_HatcheryVsGambier_OK_dat_P_0_000001_SNP.txt > $INDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex_no_header_inputRL_PL_HatcheryVsGambier_OK_dat_P_0_000001.vcf
+awk 'FNR==NR {a[$1]=$0; next}; $1 in a {print a[$1]}' $PATH_VCF/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex_no_header_inputRL_PL.txt $INDIR/Total_HatcheryVsGambier_OK_dat_P_0_000001_SNP.txt > $INDIR/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex_no_header_inputRL_PL_HatcheryVsGambier_OK_dat_P_0_000001.vcf
 
 
 
