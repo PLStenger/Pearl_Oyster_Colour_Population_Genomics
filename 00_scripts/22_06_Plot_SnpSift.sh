f7db88e9356429d251e8182a70580cdeccecbf8a
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-10 16:33:52 +0200

    Update 22_06_Plot_SnpSift.sh

--- 00_scripts/22_06_Plot_SnpSift.sh
+++ 00_scripts/22_06_Plot_SnpSift.sh
@@ -15,7 +15,7 @@ export PATH=$PATH:/appli/anaconda/2.7/bin
 #Chargement de votre environnement
 source activate /home1/datawork/plstenge/96_env_conda/glm
 
-SCRIPT_R=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/17_glm_with_iterations/22_06_Plot_SnpSift.R
+SCRIPT_R=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/22_06_Plot_SnpSift.R
 
 Rscript --vanilla $SCRIPT_R >& ${SCRIPT_FOLDER}/22_06_Plot_SnpSift.out
 
