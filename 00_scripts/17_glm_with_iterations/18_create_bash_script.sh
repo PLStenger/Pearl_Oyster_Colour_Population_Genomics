0de9602a0d9288a43e24fa8e8afb75b0b8c6ba86
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-10 19:37:27 +0200

    Update 18_create_bash_script.sh

--- 00_scripts/17_glm_with_iterations/18_create_bash_script.sh
+++ 00_scripts/17_glm_with_iterations/18_create_bash_script.sh
@@ -33,7 +33,7 @@ export PATH=\$PATH:/appli/anaconda/2.7/bin
 #Chargement de votre environnement
 source activate /home1/datawork/plstenge/96_env_conda/glm
 
-SCRIPT_R=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/${FILE##*/}
+SCRIPT_R=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/17_glm_with_iterations/${FILE##*/}
 
 Rscript --vanilla \$SCRIPT_R >& \${SCRATCH}/${FILE##*/}.out" >> $SCRIPT/${FILE##*/}.sh ;
         
