5ea0fa1735d4cd1fbbe2445c1f7379dd18db5621
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-10 15:20:20 +0200

    Update 22_05_SnpSift.sh

--- 00_scripts/22_05_SnpSift.sh
+++ 00_scripts/22_05_SnpSift.sh
@@ -14,12 +14,33 @@ DBNAME=sspace.final.scaffolds.fasta
 WORKDIR=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/09_snpEff_glm
 
 
-java -jar $SNPEFF/SnpSift.jar  \
-   extractFields $DATADIRECTORY/YellowVsGreen_OK_dat_P_0_000001_SNPEff.vcf  \
-   CHROM \
-    POS \
-   REF \
-   ALT \
- "ANN[0].EFFECT" \
-    "ANN[*].HGVS" \
-    > $DATADIRECTORY/YellowVsGreen_OK_dat_P_0_000001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/TakapotoVsGambier_OK_dat_P_0_001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/TakapotoVsGambier_OK_dat_P_0_001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/HatcheryVsGambier_OK_dat_P_0_000001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/HatcheryVsGambier_OK_dat_P_0_000001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/TakapotoVsGambier_OK_dat_P_0_01_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/TakapotoVsGambier_OK_dat_P_0_01_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/HatcheryVsGambier_OK_dat_P_0_001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/HatcheryVsGambier_OK_dat_P_0_001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/TakapotoVsHatchery_OK_dat_P_0_000001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/TakapotoVsHatchery_OK_dat_P_0_000001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/HatcheryVsGambier_OK_dat_P_0_01_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/HatcheryVsGambier_OK_dat_P_0_01_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/TakapotoVsHatchery_OK_dat_P_0_001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/TakapotoVsHatchery_OK_dat_P_0_001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/KatiuVsGambier_OK_dat_P_0_000001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/KatiuVsGambier_OK_dat_P_0_000001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/TakapotoVsHatchery_OK_dat_P_0_01_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/TakapotoVsHatchery_OK_dat_P_0_01_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/KatiuVsGambier_OK_dat_P_0_001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/KatiuVsGambier_OK_dat_P_0_001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/TakapotoVsKatiu_OK_dat_P_0_000001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/TakapotoVsKatiu_OK_dat_P_0_000001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/KatiuVsGambier_OK_dat_P_0_01_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/KatiuVsGambier_OK_dat_P_0_01_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/TakapotoVsKatiu_OK_dat_P_0_001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/TakapotoVsKatiu_OK_dat_P_0_001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/KatiuVsHatchery_OK_dat_P_0_000001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/KatiuVsHatchery_OK_dat_P_0_000001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/TakapotoVsKatiu_OK_dat_P_0_01_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/TakapotoVsKatiu_OK_dat_P_0_01_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/KatiuVsHatchery_OK_dat_P_0_001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/KatiuVsHatchery_OK_dat_P_0_001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/YellowVsGreen_OK_dat_P_0_000001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/YellowVsGreen_OK_dat_P_0_000001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/KatiuVsHatchery_OK_dat_P_0_01_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/KatiuVsHatchery_OK_dat_P_0_01_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/YellowVsGreen_OK_dat_P_0_001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/YellowVsGreen_OK_dat_P_0_001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/RedVsGreen_OK_dat_P_0_000001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/RedVsGreen_OK_dat_P_0_000001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/YellowVsGreen_OK_dat_P_0_01_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/YellowVsGreen_OK_dat_P_0_01_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/RedVsGreen_OK_dat_P_0_001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/RedVsGreen_OK_dat_P_0_001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/YellowVsRed_OK_dat_P_0_000001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/YellowVsRed_OK_dat_P_0_000001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/RedVsGreen_OK_dat_P_0_01_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/RedVsGreen_OK_dat_P_0_01_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/YellowVsRed_OK_dat_P_0_001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/YellowVsRed_OK_dat_P_0_001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/TakapotoVsGambier_OK_dat_P_0_000001_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/TakapotoVsGambier_OK_dat_P_0_000001_SNPEff_SnpSift.txt
+java -jar $SNPEFF/SnpSift.jar extractFields $DATADIRECTORY/YellowVsRed_OK_dat_P_0_01_SNPEff.vcf CHROM POS REF ALT "ANN[0].EFFECT" "ANN[*].HGVS" > $DATADIRECTORY/YellowVsRed_OK_dat_P_0_01_SNPEff_SnpSift.txt
+
+
+
