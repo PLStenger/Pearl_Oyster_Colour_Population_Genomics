a26b3b47e1ce680eaa2b7843cf56c3a85ad3bcf1
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-10 19:33:24 +0200

    Create 18_create_bash_script.sh

---
+++ 00_scripts/17_glm_with_iterations/18_create_bash_script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#!/usr/bin/env bash
+#PBS -q mpi
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=28:mem=115g
+
+
+
+DATADIRECTORY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_01_vcf_files_modified
+#DATAOUTPUT=/home1/datahome/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_fastqc_raw_data
+SCRIPT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/17_glm_with_iterations
+#HEADER=/home1/datahome/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/header.txt
+
+
+
+
+for FILE in $(ls $DATADIRECTORY/*.R)
+do
+
+echo "#!/usr/bin/env bash
+#PBS -q mpi
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=28:mem=115g
+
+DATAWORK=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_01_vcf_files_modified
+
+cd \$DATAWORK
+
+#Chargement de conda
+export PATH=\$PATH:/appli/anaconda/2.7/bin
+
+#Chargement de votre environnement
+source activate /home1/datawork/plstenge/96_env_conda/glm
+
+SCRIPT_R=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/${FILE##*/}
+
+Rscript --vanilla \$SCRIPT_R >& \${SCRATCH}/17_00_03_glm_Tuckey_with_iteration_00.out" >> $SCRIPT/${FILE##*/}.sh ;
+        
+
+done ;
+
+
