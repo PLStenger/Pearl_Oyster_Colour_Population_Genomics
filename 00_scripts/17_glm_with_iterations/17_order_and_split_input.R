48f5e06139303781dd2b3d1ace2bebae1e745895
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-10 13:30:37 +0200

    Create 17_order_and_split_input.R

---
+++ 00_scripts/17_glm_with_iterations/17_order_and_split_input.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+
+setwd("/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_01_vcf_files_modified/")
+
+
+df <- read.table("input_glm_tuckey_without_iteration.txt", header=F, skip=1)
+df2 <- df[order(df$SNP),]
+names(df2) <- NULL
+write.table(df2, file ="input_glm_tuckey_without_iteration_order.txt")
+system('split -d -l 2217162 input_glm_tuckey_without_iteration_order.txt input_glm_tuckey_without_iteration_order_split_')
