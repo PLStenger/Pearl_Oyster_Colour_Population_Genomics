ff40b4e6bdd26c0da42800ea774a7bed66e4ebac
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-13 16:04:02 +0200

    Create 21_find_for_2nd_run.sh

---
+++ 00_scripts/17_glm_with_iterations/21_find_for_2nd_run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#PBS -q mpi
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=28:mem=115g
+
+
+DATAWORK=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_01_vcf_files_modified
+OUT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/17_glm_with_iterations
+
+cd $DATAWORK
+
+#Chargement de conda
+export PATH=$PATH:/appli/anaconda/2.7/bin
+
+#Chargement de votre environnement
+source activate /home1/datawork/plstenge/96_env_conda/glm
+
+SCRIPT_R=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/17_glm_with_iterations/21_find_for_2nd_run.R
+
+Rscript --vanilla $SCRIPT_R >& $OUT/21_find_for_2nd_run.out
