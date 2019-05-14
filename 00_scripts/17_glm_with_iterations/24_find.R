71f60bfa92f8ec0be88259c235b559873d7a64dd
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-14 09:07:21 +0200

    Create 24_find.R

---
+++ 00_scripts/17_glm_with_iterations/24_find.R
@@ -0,0 +1,11 @@
+#!/usr/bin/env Rscript
+
+setwd("/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_01_vcf_files_modified")
+
+ok <- read.table("input_glm_tuckey_without_iteration_order_for_second_round_results_01.txt_ok.txt", header=F)
+head(ok)
+all <- read.table("input_glm_tuckey_without_iteration_order_for_second_round.txt", header=F, skip=1)
+head(all)
+
+for_rererererun <- all[!all$V2 %in% ok$V2, , drop = FALSE]
+write.table(for_rererererun, "input_glm_tuckey_without_iteration_order_for_third_round.txt")
