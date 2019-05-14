cfcc7449124ce7fe6aa640af92eeea8522fb770b
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-14 12:45:45 +0200

    Update 28_find_significant_SNP.sh

--- 00_scripts/17_glm_with_iterations/28_find_significant_SNP.sh
+++ 00_scripts/17_glm_with_iterations/28_find_significant_SNP.sh
@@ -16,4 +16,4 @@ source activate /home1/datawork/plstenge/96_env_conda/glm
 
 SCRIPT_R=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/00_scripts/17_glm_with_iterations/28_find_significant_SNP.R
 
-Rscript --vanilla $SCRIPT_R >& ${SCRIPT_FOLDER}/28_find_significant_SNP.out
+Rscript --vanilla $SCRIPT_R 2>&1 ${SCRIPT_FOLDER}/28_find_significant_SNP.out
