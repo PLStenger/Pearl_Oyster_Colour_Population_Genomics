52e663f9615a03b2ecc7c508ec8b109c4e755ed4
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-15 11:24:15 +0200

    Update 30_03_SNPEff_run.sh

--- 00_scripts/17_glm_with_iterations/30_03_SNPEff_run.sh
+++ 00_scripts/17_glm_with_iterations/30_03_SNPEff_run.sh
@@ -3,27 +3,37 @@
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=28:mem=115g
 
-WORKDIR=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/09_snpEff_glm_without_iteration
-INPUT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/08_02_input_pvalued_without_iteration
-#SNPEFFENV=". /appli/bioinfo/snpeff/latest/env.sh"
+DATADIRECTORY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/09_snpEff_glm_without_iteration
+#INPUT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/08_02_input_pvalued_without_iteration
+SNPEFFENV=". /appli/bioinfo/snpeff/latest/env.sh"
 
 # il faut d'abord mettre sur le fichier config la configuration du génome
 
 
 SNPEFF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/95_software/snpEff #/Path/to/snpEff/Folder
 DBNAME=sspace.final.scaffolds.fasta
+WORKDIR=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/09_snpEff_glm_without_iteration
 
 
+
+HatcheryVsGambier_dat_P_0_000000001=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/08_02_input_pvalued_without_iteration/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex_header_HatcheryVsGambier_dat_P_0_000000001.txt.vcf
 
 cd $SNPEFF
 
-for FILE in $(ls $INPUT/*.vcf)
-do
 
-java -Xmx115G -jar $SNPEFF/snpEff.jar $DBNAME $INPUT/${FILE##*/} > $WORKDIR/$OUTDIR/${FILE##*/}_SNPEff.vcf &&
-mv snpEff_summary.html $WORKDIR/$OUTDIR/${FILE##*/}_SNPEff_report.html
-mv snpEff_genes.txt $WORKDIR/$OUTDIR/${FILE##*/}_SNPEff_genes_summary.txt
 
-done ;
+java -Xmx115G -jar $SNPEFF/snpEff.jar $DBNAME $HatcheryVsGambier_dat_P_0_000000001 > $WORKDIR/$OUTDIR/HatcheryVsGambier_OK_dat_P_0_000001_SNPEff.vcf &&
+mv snpEff_summary.html $WORKDIR/$OUTDIR/HatcheryVsGambier_OK_dat_P_0_000001_report.html
+mv snpEff_genes.txt $WORKDIR/$OUTDIR/HatcheryVsGambier_OK_dat_P_0_000001_genes_summary.txt
+
+
+#for FILE in $(ls $DATADIRECTORY/*.vcf)
+#do
+
+#java -Xmx115G -jar $SNPEFF/snpEff.jar $DBNAME $DATADIRECTORY/${FILE##*/} > $WORKDIR/$OUTDIR/${FILE##*/}_SNPEff.vcf &&
+#mv snpEff_summary.html $WORKDIR/$OUTDIR/${FILE##*/}_SNPEff_report.html
+#mv snpEff_genes.txt $WORKDIR/$OUTDIR/${FILE##*/}_SNPEff_genes_summary.txt
+
+#done ;
 
 
