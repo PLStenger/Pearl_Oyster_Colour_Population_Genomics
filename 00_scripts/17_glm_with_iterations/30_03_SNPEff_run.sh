ad026c57a9c4f1c6843d05cf35110723e72f4394
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-15 11:16:05 +0200

    Update 30_03_SNPEff_run.sh

--- 00_scripts/17_glm_with_iterations/30_03_SNPEff_run.sh
+++ 00_scripts/17_glm_with_iterations/30_03_SNPEff_run.sh
@@ -3,7 +3,7 @@
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=28:mem=115g
 
-OUTPUT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/09_snpEff_glm_without_iteration
+WORKDIR=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/09_snpEff_glm_without_iteration
 INPUT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/08_02_input_pvalued_without_iteration
 #SNPEFFENV=". /appli/bioinfo/snpeff/latest/env.sh"
 
@@ -20,9 +20,9 @@ cd $SNPEFF
 for FILE in $(ls $INPUT/*.vcf)
 do
 
-java -Xmx115G -jar $SNPEFF/snpEff.jar $DBNAME $INPUT/${FILE##*/} > $OUTPUT/${FILE##*/}_SNPEff.vcf &&
-mv snpEff_summary.html $OUTPUT/${FILE##*/}_SNPEff_report.html
-mv snpEff_genes.txt $OUTPUT/${FILE##*/}_SNPEff_genes_summary.txt
+java -Xmx115G -jar $SNPEFF/snpEff.jar $DBNAME $INPUT/${FILE##*/} > $WORKDIR/$OUTDIR/${FILE##*/}_SNPEff.vcf &&
+mv snpEff_summary.html $WORKDIR/$OUTDIR/${FILE##*/}_SNPEff_report.html
+mv snpEff_genes.txt $WORKDIR/$OUTDIR/${FILE##*/}_SNPEff_genes_summary.txt
 
 done ;
 
