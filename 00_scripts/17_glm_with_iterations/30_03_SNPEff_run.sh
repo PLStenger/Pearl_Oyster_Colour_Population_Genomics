287b27375ebf09a635153550ba99d3b824749387
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-15 11:07:17 +0200

    Update 30_03_SNPEff_run.sh

--- 00_scripts/17_glm_with_iterations/30_03_SNPEff_run.sh
+++ 00_scripts/17_glm_with_iterations/30_03_SNPEff_run.sh
@@ -3,7 +3,8 @@
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=28:mem=115g
 
-DATADIRECTORY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/09_snpEff_glm_without_iteration
+OUTPUT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/09_snpEff_glm_without_iteration
+INPUT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/08_02_input_pvalued_without_iteration
 #SNPEFFENV=". /appli/bioinfo/snpeff/latest/env.sh"
 
 # il faut d'abord mettre sur le fichier config la configuration du génome
@@ -16,12 +17,12 @@ DBNAME=sspace.final.scaffolds.fasta
 
 cd $SNPEFF
 
-for FILE in $(ls $DATADIRECTORY/*.vcf)
+for FILE in $(ls $INPUT/*.vcf)
 do
 
-java -Xmx115G -jar $SNPEFF/snpEff.jar $DBNAME $DATADIRECTORY/${FILE##*/} > $DATADIRECTORY/${FILE##*/}_SNPEff.vcf &&
-mv snpEff_summary.html $DATADIRECTORY/${FILE##*/}_SNPEff_report.html
-mv snpEff_genes.txt DATADIRECTORY/${FILE##*/}_SNPEff_genes_summary.txt
+java -Xmx115G -jar $SNPEFF/snpEff.jar $DBNAME $INPUT/${FILE##*/} > $OUTPUT/${FILE##*/}_SNPEff.vcf &&
+mv snpEff_summary.html $OUTPUT/${FILE##*/}_SNPEff_report.html
+mv snpEff_genes.txt $OUTPUT/${FILE##*/}_SNPEff_genes_summary.txt
 
 done ;
 
