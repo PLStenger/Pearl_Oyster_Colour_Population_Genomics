85ed780fadd67346cc52e4ac0d3bc854f9125d7b
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-16 16:21:27 +0200

    Update 33_snpSift.sh

--- 00_scripts/17_glm_with_iterations/33_snpSift.sh
+++ 00_scripts/17_glm_with_iterations/33_snpSift.sh
@@ -3,11 +3,11 @@
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=28:mem=115g
 
-DATA_DIRECTORY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/09_snpEff_glm_without_iteration
+DATADIRECTORY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/09_snpEff_glm_without_iteration
 SNPEFF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/95_software/SNPEff_2/snpEff #/Path/to/snpEff/Folder
 DBNAME=sspace.final.scaffolds.fasta
 
-cd $DATA_DIRECTORY
+cd $DATADIRECTORY
 
 for FILE in $(ls $DATADIRECTORY/*.vcf)
 do
