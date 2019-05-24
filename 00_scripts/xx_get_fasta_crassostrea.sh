6aaa8d23c849484f8b72745e5598fe125c419828
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-24 09:34:07 +0200

    Update xx_get_fasta_crassostrea.sh

--- 00_scripts/xx_get_fasta_crassostrea.sh
+++ 00_scripts/xx_get_fasta_crassostrea.sh
@@ -5,7 +5,8 @@
 
 DATADIRECTORY=/home1/datawork/plstenge/crassostrea
 GENOME_FASTA=/home1/datawork/plstenge/crassostrea/oyster_v9_AMP_enriched.fa.fasta
-GFF=/home1/datawork/plstenge/crassostrea/Oyster_V9_AMP_sorted_gff.gff
+#GFF=/home1/datawork/plstenge/crassostrea/Oyster_V9_AMP_sorted_gff.gff
+GFF=/home1/datawork/plstenge/crassostrea/X_Oyster_V9_AMP_sorted_gff_CDS.gff
 
 # https://bedtools.readthedocs.io/en/latest/content/tools/getfasta.html
 
@@ -16,5 +17,5 @@ $BEDTOOLS_ENV
 cd $DATADIRECTORY
 
 #bedtools getfasta -fi $GENOME_FASTA -bed $GFF > $DATADIRECTORY/oyster_v9_AMP_enriched_ok.fasta
-bedtools getfasta -fi $GENOME_FASTA -bed $GFF -split > $DATADIRECTORY/oyster_v9_AMP_enriched_ok.fasta
+bedtools getfasta -fi $GENOME_FASTA -bed $GFF -split > $DATADIRECTORY/X_Oyster_V9_AMP_sorted_gff_CDS.fasta
 
