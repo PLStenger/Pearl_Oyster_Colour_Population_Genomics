654f6548ea42818013c3b53a69d342ca63170fcf
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-06-19 10:10:46 +0200

    Update 38_blast_against_swissprot.sh

--- 00_scripts/17_glm_with_iterations/38_blast_against_swissprot.sh
+++ 00_scripts/17_glm_with_iterations/38_blast_against_swissprot.sh
@@ -3,6 +3,9 @@
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=28:mem=115g
 
+export PATH=$PATH:/appli/anaconda/2.7/bin
+source activate /home1/datawork/plstenge/96_env_conda/freebayes
+
 # Global variables
 SEQUENCE_FILE=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/10_00_annotation_go/go_enrichment-master/03_sequences/analyzed_genes.fasta
 SWISSPROT_RESULT=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/10_00_annotation_go/go_enrichment-master/04_blast_results/analyzed_genes.swissprot
@@ -11,8 +14,8 @@ SWISSPROT_DB=/home/ref-bioinfo/beedeem/p/Uniprot_SwissProt
 
 # Blast all sequences against swissprot (must be installed locally)
 # WARNING use `-j N` if you need to limit the number of CPUs used to N <integer>
-#cat $SEQUENCE_FILE | parallel -k --block 1k --recstart '>' --pipe 'blastx -db '$SWISSPROT_DB' -query - -evalue 1e-3 -outfmt 6 -max_target_seqs 1' > $SWISSPROT_RESULT
-cat $SEQUENCE_FILE | --pipe 'blastx -db '$SWISSPROT_DB' -query - -evalue 1e-3 -outfmt 6 -max_target_seqs 1' > $SWISSPROT_RESULT
+cat $SEQUENCE_FILE | parallel -k --block 1k --recstart '>' --pipe 'blastx -db '$SWISSPROT_DB' -query - -evalue 1e-3 -outfmt 6 -max_target_seqs 1' > $SWISSPROT_RESULT
+#cat $SEQUENCE_FILE | --pipe 'blastx -db '$SWISSPROT_DB' -query - -evalue 1e-3 -outfmt 6 -max_target_seqs 1' > $SWISSPROT_RESULT
 
 
 # TODO filter blasts on similarity, evalue, length...
