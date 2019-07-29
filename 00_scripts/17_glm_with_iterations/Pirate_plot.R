9e19fb88a2a7f7da036c8444f5e8fcf2edec2991
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-07-29 20:01:56 +0200

    Create Pirate_plot.R

---
+++ 00_scripts/17_glm_with_iterations/Pirate_plot.R
@@ -0,0 +1,42 @@
+#!/usr/bin/env Rscript
+
+setwd("/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/07_01_vcf_files_modified")
+
+library("yarrr")
+
+data <- read.table("AF_pool10ER.txt", skip=1)
+datb <- read.table("AF_pool11EJ.txt", skip=1)
+datc <- read.table("AF_pool12EV.txt", skip=1)
+datd <- read.table("AF_pool1TR.txt", skip=1)
+date <- read.table("AF_pool2TJ.txt", skip=1)
+datf <- read.table("AF_pool3TV.txt", skip=1)
+datg <- read.table("AF_pool4GR.txt", skip=1)
+dath <- read.table("AF_pool5GJ.txt", skip=1)
+dati <- read.table("AF_pool6GV.txt", skip=1)
+datj <- read.table("AF_pool7KR.txt", skip=1)
+datk <- read.table("AF_pool8KJ.txt", skip=1)
+datl <- read.table("AF_pool9KV.txt", skip=1)
+
+
+dat <- data.frame(datd$V2, datj$V2, datg$V2, data$V2, date$V2, datk$V2, dath$V2, datb$V2, datf$V2, datl$V2, dati$V2, datc$V2)
+colnames(dat) <- c("Takapoto_R", "Katiu_R", "Gambier_R", "Hatchery_R", "Takapoto_Y", "Katiu_Y", "Gambier_Y", "Hatchery_Y", "Takapoto_G", "Katiu_G", "Gambier_G", "Hatchery_G")
+head(dat)
+str(dat)
+
+dat3 <- gather(dat)
+head(dat3)
+
+pdf(file = "pirateplot.pdf", width = 15, height = 6, pointsize = 10)
+
+pirateplot(formula = value ~ key,
+           data = dat3,
+           xlab = "Populations",
+           ylab = "Allelic frequencies",
+           pal = c("red", "red","red", "red","yellow", "yellow","yellow", "yellow","forestgreen", "forestgreen","forestgreen", "forestgreen"),
+           sortx = "sequential")
+           
+dev.off()
+
+kruskal.test(key ~ value, data = dat3) 
+
+pairwise.wilcox.test(dat3$value, dat3$key, p.adjust.method = "BH")
