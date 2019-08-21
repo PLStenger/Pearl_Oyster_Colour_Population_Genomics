b760115be4cf19c0ec72e056952cc4f94ce10aff
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-08-21 20:02:27 +0200

    Update 32_getfasta_for_db.sh

--- 00_scripts/17_glm_with_iterations/32_getfasta_for_db.sh
+++ 00_scripts/17_glm_with_iterations/32_getfasta_for_db.sh
@@ -7,7 +7,12 @@ DATADIRECTORY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/0
 GENOME_FASTA=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds.fasta
 #GFF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds.gff3
 #GFF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds_54K_CDS_3.gff3
-GFF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/porpho.gff3
+#GFF=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/porpho.gff3
+GFF1=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/shematrin.gff3
+GFF2=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/cyto.gff3
+GFF3=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/cadherin.gff3
+
+
 
 # https://bedtools.readthedocs.io/en/latest/content/tools/getfasta.html
 
@@ -17,5 +22,7 @@ $BEDTOOLS_ENV
 
 cd $DATADIRECTORY
 
-bedtools getfasta -fi $GENOME_FASTA -bed $GFF > $DATADIRECTORY/porpho.fasta
+bedtools getfasta -fi $GENOME_FASTA -bed $GFF1 > $DATADIRECTORY/shematrin.fasta
+bedtools getfasta -fi $GENOME_FASTA -bed $GFF2 > $DATADIRECTORY/cyto.fasta
+bedtools getfasta -fi $GENOME_FASTA -bed $GFF3 > $DATADIRECTORY/cadherin.fasta
 
