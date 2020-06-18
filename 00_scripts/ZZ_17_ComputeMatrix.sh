d3072394af28a21d268f398849534763066cd0fc
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-06-18 14:13:14 +1100

    Update ZZ_17_ComputeMatrix.sh

--- 00_scripts/ZZ_17_ComputeMatrix.sh
+++ 00_scripts/ZZ_17_ComputeMatrix.sh
@@ -13,8 +13,32 @@ source activate $DATAWORK/96_env_conda/deeptools
 
 cd $DATADIRECTORY
 
-computeMatrixOperations cbind -m FC_A1vsA3_down_Acclimation_1_31_5_rbind_matrix.mat.gz FC_A1vsA3_down_Acclimation_3_30_rbind_matrix.mat.gz -o FC_A1vsA3_down_Acclimation_cbind_matrix.mat.gz
+#computeMatrixOperations cbind -m FC_A1vsA3_down_Acclimation_1_31_5_rbind_matrix.mat.gz FC_A1vsA3_down_Acclimation_3_30_rbind_matrix.mat.gz -o FC_A1vsA3_down_Acclimation_cbind_matrix.mat.gz
 
-plotHeatmap -m FC_A1vsA3_down_Acclimation_cbind_matrix.mat.gz -out FC_A1vsA3_down_Acclimation_cbind_matrix.mat.gz.pdf
+#plotHeatmap -m FC_A1vsA3_down_Acclimation_cbind_matrix.mat.gz -out FC_A1vsA3_down_Acclimation_cbind_matrix.mat.gz.pdf
 
+
+computeMatrixOperations cbind -m FC_A1vsA3_down_Acclimation_1_31_5_rbind_matrix.mat.gz FC_A1vsA3_down_Acclimation_3_30_rbind_matrix.mat.gz FC_A1vsA3_down_Control_1_30_rbind_matrix.mat.gz FC_A1vsA3_down_Control_3_30_rbind_matrix.mat.gz FC_A1vsA3_up_Acclimation_1_31_5_rbind_matrix.mat.gz FC_A1vsA3_up_Acclimation_3_30_rbind_matrix.mat.gz FC_A1vsA3_up_Control_1_30_rbind_matrix.mat.gz FC_A1vsA3_up_Control_3_30_rbind_matrix.mat.gz -o FC_A1vsA3.mat.gz
+
+plotHeatmap -m FC_A1vsA3.mat.gz -out FC_A1vsA3.mat.gz.pdf
+
+computeMatrixOperations cbind -m FC_A1vsC3_down_Acclimation_1_31_5_rbind_matrix.mat.gz FC_A1vsC3_down_Acclimation_3_30_rbind_matrix.mat.gz FC_A1vsC3_down_Control_1_30_rbind_matrix.mat.gz FC_A1vsC3_down_Control_3_30_rbind_matrix.mat.gz FC_A1vsC3_up_Acclimation_1_31_5_rbind_matrix.mat.gz FC_A1vsC3_up_Acclimation_3_30_rbind_matrix.mat.gz FC_A1vsC3_up_Control_1_30_rbind_matrix.mat.gz FC_A1vsC3_up_Control_3_30_rbind_matrix.mat.gz -o FC_A1vsC3.mat.gz
+
+plotHeatmap -m FC_A1vsC3.mat.gz -out FC_A1vsC3.mat.gz.pdf
+
+computeMatrixOperations cbind -m FC_C1vsA1_down_Acclimation_1_31_5_rbind_matrix.mat.gz FC_C1vsA1_down_Acclimation_3_30_rbind_matrix.mat.gz FC_C1vsA1_down_Control_1_30_rbind_matrix.mat.gz FC_C1vsA1_down_Control_3_30_rbind_matrix.mat.gz FC_C1vsA1_up_Acclimation_1_31_5_rbind_matrix.mat.gz FC_C1vsA1_up_Acclimation_3_30_rbind_matrix.mat.gz FC_C1vsA1_up_Control_1_30_rbind_matrix.mat.gz FC_C1vsA1_up_Control_3_30_rbind_matrix.mat.gz -o FC_C1vsA1.mat.gz
+
+plotHeatmap -m FC_A1vsC3.mat.gz -out FC_A1vsC3.mat.gz.pdf
+
+computeMatrixOperations cbind -m FC_C1vsA3_down_Acclimation_1_31_5_rbind_matrix.mat.gz FC_C1vsA3_down_Acclimation_3_30_rbind_matrix.mat.gz FC_C1vsA3_down_Control_1_30_rbind_matrix.mat.gz FC_C1vsA3_down_Control_3_30_rbind_matrix.mat.gz FC_C1vsA3_up_Acclimation_1_31_5_rbind_matrix.mat.gz FC_C1vsA3_up_Acclimation_3_30_rbind_matrix.mat.gz FC_C1vsA3_up_Control_1_30_rbind_matrix.mat.gz FC_C1vsA3_up_Control_3_30_rbind_matrix.mat.gz -o FC_C1vsA3.mat.gz
+
+plotHeatmap -m FC_A1vsC3.mat.gz -out FC_A1vsC3.mat.gz.pdf
+
+computeMatrixOperations cbind -m FC_C1vsC3_down_Acclimation_1_31_5_rbind_matrix.mat.gz FC_C1vsC3_down_Acclimation_3_30_rbind_matrix.mat.gz FC_C1vsC3_down_Control_1_30_rbind_matrix.mat.gz FC_C1vsC3_down_Control_3_30_rbind_matrix.mat.gz FC_C1vsC3_up_Acclimation_1_31_5_rbind_matrix.mat.gz FC_C1vsC3_up_Acclimation_3_30_rbind_matrix.mat.gz FC_C1vsC3_up_Control_1_30_rbind_matrix.mat.gz FC_C1vsC3_up_Control_3_30_rbind_matrix.mat.gz -o FC_C1vsC3.mat.gz
+
+plotHeatmap -m FC_A1vsC3.mat.gz -out FC_A1vsC3.mat.gz.pdf
+
+computeMatrixOperations cbind -m FC_C3vsA3_down_Acclimation_1_31_5_rbind_matrix.mat.gz FC_C3vsA3_down_Acclimation_3_30_rbind_matrix.mat.gz FC_C3vsA3_down_Control_1_30_rbind_matrix.mat.gz FC_C3vsA3_down_Control_3_30_rbind_matrix.mat.gz FC_C3vsA3_up_Acclimation_1_31_5_rbind_matrix.mat.gz FC_C3vsA3_up_Acclimation_3_30_rbind_matrix.mat.gz FC_C3vsA3_up_Control_1_30_rbind_matrix.mat.gz FC_C3vsA3_up_Control_3_30_rbind_matrix.mat.gz -o FC_C3vsA3.mat.gz
+
+plotHeatmap -m FC_A1vsC3.mat.gz -out FC_A1vsC3.mat.gz.pdf
 
