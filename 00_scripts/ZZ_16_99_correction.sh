c0a8f0155c937e8a051d5402721c29259bf945ba
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-06-18 16:08:59 +1100

    Update ZZ_16_99_correction.sh

--- 00_scripts/ZZ_16_99_correction.sh
+++ 00_scripts/ZZ_16_99_correction.sh
@@ -13,16 +13,30 @@ source activate $DATAWORK/96_env_conda/deeptools
 
 cd $DATADIRECTORY
 
-computeMatrix scale-regions -S Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_C1vsA3_up.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_C1vsA3_up_Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
+#computeMatrix scale-regions -S Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_C1vsA3_up.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_C1vsA3_up_Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
 
-plotHeatmap -m FC_C1vsA3_up_Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C1vsA3_up_Acclimation_1_31_5b_heatmap.pdf 
+#plotHeatmap -m FC_C1vsA3_up_Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C1vsA3_up_Acclimation_1_31_5b_heatmap.pdf 
 
-computeMatrixOperations cbind -m FC_C1vsA3_up_Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz FC_C1vsA3_up_Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz FC_C1vsA3_up_Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -o FC_C1vsA3_up_Control_3_30_cbind_matrix.mat.gz
+#computeMatrixOperations cbind -m FC_C1vsA3_up_Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz FC_C1vsA3_up_Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz FC_C1vsA3_up_Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -o FC_C1vsA3_up_Control_3_30_cbind_matrix.mat.gz
 
-plotHeatmap -m FC_C1vsA3_up_Control_3_30_cbind_matrix.mat.gz -o FC_C1vsA3_up_Control_3_30_matrix.mat.gz_cbind.pdf
+#plotHeatmap -m FC_C1vsA3_up_Control_3_30_cbind_matrix.mat.gz -o FC_C1vsA3_up_Control_3_30_matrix.mat.gz_cbind.pdf
 
-computeMatrixOperations rbind -m FC_C1vsA3_up_Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz FC_C1vsA3_up_Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz FC_C1vsA3_up_Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -o FC_C1vsA3_up_Control_3_30_rbind_matrix.mat.gz
+#computeMatrixOperations rbind -m FC_C1vsA3_up_Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz FC_C1vsA3_up_Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz FC_C1vsA3_up_Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -o FC_C1vsA3_up_Control_3_30_rbind_matrix.mat.gz
 
-plotHeatmap -m FC_C1vsA3_up_Control_3_30_rbind_matrix.mat.gz -o FC_C1vsA3_up_Control_3_30_matrix.mat.gz_rbind.pdf
+#plotHeatmap -m FC_C1vsA3_up_Control_3_30_rbind_matrix.mat.gz -o FC_C1vsA3_up_Control_3_30_matrix.mat.gz_rbind.pdf
+
+
+
+
+plotHeatmap -m FC_C1vsA1_down.mat.gz -out FC_C1vsA1_down.mat.gz.pdf
+plotHeatmap -m FC_C1vsA3_down.mat.gz -out FC_C1vsA3_down.mat.gz.pdf
+plotHeatmap -m FC_C1vsC3_down.mat.gz -out FC_C1vsC3_down.mat.gz.pdf
+plotHeatmap -m FC_C3vsA3_down.mat.gz -out FC_C3vsA3_down.mat.gz.pdf
+plotHeatmap -m FC_C1vsA1_up.mat.gz -out FC_C1vsA1_up.mat.gz.pdf
+plotHeatmap -m FC_C1vsA3_up.mat.gz -out FC_C1vsA3_up.mat.gz.pdf
+plotHeatmap -m FC_C1vsC3_up.mat.gz -out FC_C1vsC3_up.mat.gz.pdf
+plotHeatmap -m FC_C3vsA3_up.mat.gz -out FC_C3vsA3_up.mat.gz.pdf
+
+
 
 
