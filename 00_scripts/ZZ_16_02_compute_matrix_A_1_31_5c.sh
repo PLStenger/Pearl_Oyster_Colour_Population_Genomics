3b08a1ee4f228259b560f7c8bc6094a9e61be004
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-06-19 11:52:11 +1100

    Update ZZ_16_02_compute_matrix_A_1_31_5c.sh

--- 00_scripts/ZZ_16_02_compute_matrix_A_1_31_5c.sh
+++ 00_scripts/ZZ_16_02_compute_matrix_A_1_31_5c.sh
@@ -17,16 +17,16 @@ cd $DATADIRECTORY
 
 #computeMatrix scale-regions -S Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R scaffold_gene_lenght_02.bed  --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
 
-#plotProfile -m Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out Acclimation_1_31_5b.pdf --plotTitle "Acclimation_1_31_5b" --plotHeight 10 --plotWidth 10
+#plotProfile -m Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out Acclimation_1_31_5c.pdf --plotTitle "Acclimation_1_31_5c" --plotHeight 10 --plotWidth 10
 
-#plotHeatmap -m Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out Acclimation_1_31_5b_heatmap.pdf 
+#plotHeatmap -m Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out Acclimation_1_31_5c_heatmap.pdf 
 
 #######################################################################################################################################################################
 #######################################################################################################################################################################
 
 computeMatrix scale-regions -S Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_A1vsA3_down.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_A1vsA3_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
 
-plotHeatmap -m FC_A1vsA3_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_A1vsA3_down_Acclimation_1_31_5b_heatmap.pdf 
+plotHeatmap -m FC_A1vsA3_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_A1vsA3_down_Acclimation_1_31_5c_heatmap.pdf 
 
 #######################################################################################################################################################################
 
@@ -39,7 +39,7 @@ plotHeatmap -m FC_A1vsA3_up_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.dedupli
 
 computeMatrix scale-regions -S Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_A1vsC3_down.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_A1vsC3_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
 
-plotHeatmap -m FC_A1vsC3_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_A1vsC3_down_Acclimation_1_31_5b_heatmap.pdf 
+plotHeatmap -m FC_A1vsC3_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_A1vsC3_down_Acclimation_1_31_5c_heatmap.pdf 
 
 #######################################################################################################################################################################
 
@@ -52,7 +52,7 @@ plotHeatmap -m FC_A1vsC3_up_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.dedupli
 
 computeMatrix scale-regions -S Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_C1vsA1_down.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_C1vsA1_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
 
-plotHeatmap -m FC_C1vsA1_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C1vsA1_down_Acclimation_1_31_5b_heatmap.pdf 
+plotHeatmap -m FC_C1vsA1_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C1vsA1_down_Acclimation_1_31_5c_heatmap.pdf 
 
 #######################################################################################################################################################################
 
@@ -65,7 +65,7 @@ plotHeatmap -m FC_C1vsA1_up_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.dedupli
 
 computeMatrix scale-regions -S Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_C1vsA3_down.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_C1vsA3_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
 
-plotHeatmap -m FC_C1vsA3_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C1vsA3_down_Acclimation_1_31_5b_heatmap.pdf 
+plotHeatmap -m FC_C1vsA3_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C1vsA3_down_Acclimation_1_31_5c_heatmap.pdf 
 
 #######################################################################################################################################################################
 
@@ -78,7 +78,7 @@ plotHeatmap -m FC_C1vsA3_up_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.dedupli
 
 computeMatrix scale-regions -S Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_C1vsC3_down.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_C1vsC3_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
 
-plotHeatmap -m FC_C1vsC3_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C1vsC3_down_Acclimation_1_31_5b_heatmap.pdf 
+plotHeatmap -m FC_C1vsC3_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C1vsC3_down_Acclimation_1_31_5c_heatmap.pdf 
 
 #######################################################################################################################################################################
 
@@ -91,7 +91,7 @@ plotHeatmap -m FC_C1vsC3_up_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.dedupli
 
 computeMatrix scale-regions -S Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_C3vsA3_down.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_C3vsA3_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
 
-plotHeatmap -m FC_C3vsA3_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C3vsA3_down_Acclimation_1_31_5b_heatmap.pdf 
+plotHeatmap -m FC_C3vsA3_down_Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C3vsA3_down_Acclimation_1_31_5c_heatmap.pdf 
 
 #######################################################################################################################################################################
 
