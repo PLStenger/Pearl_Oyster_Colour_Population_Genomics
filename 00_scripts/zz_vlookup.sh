64b6fc02ee9fe4bb5b3a1305bfad3e1fb42fdd2f
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-06-22 18:55:00 +1100

    Update zz_vlookup.sh

--- 00_scripts/zz_vlookup.sh
+++ 00_scripts/zz_vlookup.sh
@@ -15,18 +15,9 @@ cd /home1/scratch/plstenge/genome
 #    print
 #}' selected_snp_for_validation.txt sspace.final.scaffolds_database.txt > selected_snp_for_validation_with_sequences.txt
 
-#awk '
-#    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
-#    NR==FNR{a[$1]          # hash $1 of genes file to a
-#    next
-#}
-#($1 in a) {                # lookup from transcriptome
-#    print
-#}' 3-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 3-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 3-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
 
-
-for FILE in $(ls $DATADIRECTORY/*_cpg_count.txt_no_first_line_rearranged5.txt)
-do
+##############################################################################################################################
+################## upstream
 
 awk '
     # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
@@ -35,9 +26,363 @@ awk '
 }
 ($1 in a) {                # lookup from transcriptome
     print
-}' ${FILE##*/}_EVM_combined_gene_upstream_02.gff ${FILE##*/}_EVM_combined_gene_02.gff > ${FILE##*/}_EVM_combined_gene_MATCH_w_upstream.txt
+}' 1-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 1-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 1-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
 
 
-done ;
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 1-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 1-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 1-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 1-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 1-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 1-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 1-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 1-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 1-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 1-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 1-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 1-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 1-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 1-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 1-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 2-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 2-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 2-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 2-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 2-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 2-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 2-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 2-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 2-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 2-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 2-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 2-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 2-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 2-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 2-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 2-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 2-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 2-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 3-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 3-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 3-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 3-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 3-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 3-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 3-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 3-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 3-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 3-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 3-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 3-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 3-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 3-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 3-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 3-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_upstream_02.gff 3-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 3-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_upstream.txt
+
+
+
+
+##############################################################################################################################
+################## downstream
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 1-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 1-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 1-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 1-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 1-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 1-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 1-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 1-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 1-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 1-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 1-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 1-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 1-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 1-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 1-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 1-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 1-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 1-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 2-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 2-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 2-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 2-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 2-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 2-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 2-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 2-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 2-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 2-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 2-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 2-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 2-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 2-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 2-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 2-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 2-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 2-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 3-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 3-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 3-J-2_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 3-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 3-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 3-J-7_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 3-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 3-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 3-R-180_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 3-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 3-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 3-R-183_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 3-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 3-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 3-V-613_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' 3-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_downstream_02.gff 3-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_02.gff > 3-V-620_cpg_count.txt_no_first_line_rearranged5.txt_EVM_combined_gene_MATCH_w_downstream.txt
+
+
 
 
