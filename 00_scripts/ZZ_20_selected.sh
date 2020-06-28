588fb675447ff65a7b7e48f6dac70f273db5c5e2
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-06-29 10:41:41 +1100

    Update ZZ_20_selected.sh

--- 00_scripts/ZZ_20_selected.sh
+++ 00_scripts/ZZ_20_selected.sh
@@ -153,17 +153,17 @@ cd $DATADIRECTORY
 
 #computeMatrixOperations cbind -m Acclimation_1_31_5_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_A1vsA3_p0_05_rbind.bed_matrix.mat.gz Acclimation_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_A1vsA3_p0_05_rbind.bed_matrix.mat.gz Control_1_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_A1vsA3_p0_05_rbind.bed_matrix.mat.gz Control_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_A1vsA3_p0_05_rbind.bed_matrix.mat.gz -o select_A1vsA3_p0_05_rbind.bed_matrix.mat.gz 
 
-plotHeatmap -m select_A1vsA3_p0_05_rbind.bed_matrix.mat.gz \
-     -out select_A1vsA3_p0_05_rbind_kmeans_04.bed_matrix.mat.gz.pdf \
-     --colorMap RdBu \
-     --whatToShow 'plot, heatmap and colorbar' \
-     --zMin -3 --zMax 3 \
-     --kmeans 4 \
-     --outFileSortedRegions select_A1vsA3_p0_05_rbind_kmeans_04_out_clusters.bed
+#plotHeatmap -m select_A1vsA3_p0_05_rbind.bed_matrix.mat.gz \
+#     -out select_A1vsA3_p0_05_rbind_kmeans_04.bed_matrix.mat.gz.pdf \
+#     --colorMap RdBu \
+#     --whatToShow 'plot, heatmap and colorbar' \
+#     --zMin -3 --zMax 3 \
+#     --kmeans 4 \
+#     --outFileSortedRegions select_A1vsA3_p0_05_rbind_kmeans_04_out_clusters.bed
 
 #computeMatrixOperations cbind -m Acclimation_1_31_5_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_A1vsC3_p0_05_rbind.bed_matrix.mat.gz Acclimation_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_A1vsC3_p0_05_rbind.bed_matrix.mat.gz Control_1_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_A1vsC3_p0_05_rbind.bed_matrix.mat.gz Control_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_A1vsC3_p0_05_rbind.bed_matrix.mat.gz -o select_A1vsC3_p0_05_rbind.bed_matrix.mat.gz
 
-#plotHeatmap -m select_A1vsC3_p0_05_rbind.bed_matrix.mat.gz \
+plotHeatmap -m select_A1vsC3_p0_05_rbind.bed_matrix.mat.gz \
      -out select_A1vsC3_p0_05_rbind_kmeans_04.bed_matrix.mat.gz.pdf \
      --colorMap RdBu \
      --whatToShow 'plot, heatmap and colorbar' \
@@ -173,44 +173,142 @@ plotHeatmap -m select_A1vsA3_p0_05_rbind.bed_matrix.mat.gz \
 
 #computeMatrixOperations cbind -m Acclimation_1_31_5_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_rbind.bed_matrix.mat.gz Acclimation_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_rbind.bed_matrix.mat.gz Control_1_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_rbind.bed_matrix.mat.gz Control_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_rbind.bed_matrix.mat.gz -o select_C1vsA1_p0_05_rbind.bed_matrix.mat.gz
 
-plotHeatmap -m select_C1vsA1_p0_05_rbind.bed_matrix.mat.gz \
-     -out select_C1vsA1_p0_05_rbind_kmeans_04.bed_matrix.mat.gz.pdf \
-     --colorMap RdBu \
-     --whatToShow 'plot, heatmap and colorbar' \
-     --zMin -3 --zMax 3 \
-     --kmeans 4 \
-     --outFileSortedRegions select_C1vsA1_p0_05_rbind_kmeans_04_out_clusters.bed
-
+#plotHeatmap -m select_C1vsA1_p0_05_rbind.bed_matrix.mat.gz \
+#     -out select_C1vsA1_p0_05_rbind_kmeans_04.bed_matrix.mat.gz.pdf \
+#     --colorMap RdBu \
+#     --whatToShow 'plot, heatmap and colorbar' \
+#     --zMin -3 --zMax 3 \
+#     --kmeans 4 \
+#     --outFileSortedRegions select_C1vsA1_p0_05_rbind_kmeans_04_out_clusters.bed
+#
 #computeMatrixOperations cbind -m Acclimation_1_31_5_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA3_p0_05_rbind.bed_matrix.mat.gz Acclimation_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA3_p0_05_rbind.bed_matrix.mat.gz Control_1_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA3_p0_05_rbind.bed_matrix.mat.gz Control_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA3_p0_05_rbind.bed_matrix.mat.gz -o select_C1vsA3_p0_05_rbind.bed_matrix.mat.gz
 
-plotHeatmap -m select_C1vsA3_p0_05_rbind.bed_matrix.mat.gz \
-     -out select_C1vsA3_p0_05_rbind_kmeans_04.bed_matrix.mat.gz.pdf \
-     --colorMap RdBu \
-     --whatToShow 'plot, heatmap and colorbar' \
-     --zMin -3 --zMax 3 \
-     --kmeans 4 \
-     --outFileSortedRegions select_C1vsA3_p0_05_rbind_kmeans_04_out_clusters.bed
+#plotHeatmap -m select_C1vsA3_p0_05_rbind.bed_matrix.mat.gz \
+#     -out select_C1vsA3_p0_05_rbind_kmeans_04.bed_matrix.mat.gz.pdf \
+#     --colorMap RdBu \
+#     --whatToShow 'plot, heatmap and colorbar' \
+#     --zMin -3 --zMax 3 \
+#     --kmeans 4 \
+#     --outFileSortedRegions select_C1vsA3_p0_05_rbind_kmeans_04_out_clusters.bed
 
 #computeMatrixOperations cbind -m Acclimation_1_31_5_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsC3_p0_05_rbind.bed_matrix.mat.gz Acclimation_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsC3_p0_05_rbind.bed_matrix.mat.gz Control_1_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsC3_p0_05_rbind.bed_matrix.mat.gz Control_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsC3_p0_05_rbind.bed_matrix.mat.gz -o select_C1vsC3_p0_05_rbind.bed_matrix.mat.gz 
 
-plotHeatmap -m select_C1vsC3_p0_05_rbind.bed_matrix.mat.gz \
-     -out select_C1vsC3_p0_05_rbind_kmeans_04.bed_matrix.mat.gz.pdf \
-     --colorMap RdBu \
-     --whatToShow 'plot, heatmap and colorbar' \
-     --zMin -3 --zMax 3 \
-     --kmeans 4 \
-     --outFileSortedRegions select_C1vsC3_p0_05_rbind_kmeans_04_out_clusters.bed
-
+#plotHeatmap -m select_C1vsC3_p0_05_rbind.bed_matrix.mat.gz \
+#     -out select_C1vsC3_p0_05_rbind_kmeans_04.bed_matrix.mat.gz.pdf \
+#     --colorMap RdBu \
+#     --whatToShow 'plot, heatmap and colorbar' \
+#     --zMin -3 --zMax 3 \
+#     --kmeans 4 \
+#     --outFileSortedRegions select_C1vsC3_p0_05_rbind_kmeans_04_out_clusters.bed
+#
 #computeMatrixOperations cbind -m Acclimation_1_31_5_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C3vsA3_p0_05_rbind.bed_matrix.mat.gz Acclimation_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C3vsA3_p0_05_rbind.bed_matrix.mat.gz Control_1_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C3vsA3_p0_05_rbind.bed_matrix.mat.gz Control_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C3vsA3_p0_05_rbind.bed_matrix.mat.gz -o select_C3vsA3_p0_05_rbind.bed_matrix.mat.gz
 
-plotHeatmap -m select_C3vsA3_p0_05_rbind.bed_matrix.mat.gz \
-     -out select_C3vsA3_p0_05_rbind_kmeans_04.bed_matrix.mat.gz.pdf \
+#plotHeatmap -m select_C3vsA3_p0_05_rbind.bed_matrix.mat.gz \
+#     -out select_C3vsA3_p0_05_rbind_kmeans_04.bed_matrix.mat.gz.pdf \
+#     --colorMap RdBu \
+#     --whatToShow 'plot, heatmap and colorbar' \
+#     --zMin -3 --zMax 3 \
+#     --kmeans 4 \
+#     --outFileSortedRegions select_C3vsA3_p0_05_rbind_kmeans_04_out_clusters.bed
+#
+
+
+
+
+################################################################################################################################################################
+################################################################################################################################################################
+# Pour enlver les genes 26592 et 5615 qui "tirent" complement le jeu de donnée A1vsC1
+################################################################################################################################################################
+################################################################################################################################################################
+
+#sed '/gene26592/d' select_C1vsA1_p0_05.bed > select_C1vsA1_p0_05_less_gene26592.bed 
+#sed '/gene5615/d' select_C1vsA1_p0_05_less_gene26592.bed > select_C1vsA1_p0_05_less_gene26592_and_5615.bed 
+
+#sed '/gene26592/d' select_C1vsA1_p0_05_up.bed > select_C1vsA1_p0_05_up_less_gene26592.bed 
+#sed '/gene5615/d' select_C1vsA1_p0_05_up_less_gene26592.bed  > select_C1vsA1_p0_05_up_less_gene26592_and_5615.bed 
+
+for SELECTION in $(ls $DATADIRECTORY/select_*less_gene26592_and_5615.bed)
+do
+
+for FILE in $(ls $DATADIRECTORY/*.bam_sorted.bam.bw)
+do
+
+computeMatrix scale-regions -S ${FILE##*/} -R ${SELECTION##*/} --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o ${FILE##*/}_${SELECTION##*/}_matrix.mat.gz
+
+plotHeatmap -m ${FILE##*/}_${SELECTION##*/}_matrix.mat.gz -out ${FILE##*/}_${SELECTION##*/}_matrix.mat.gz.pdf 
+
+
+done ;
+done ;
+
+#### ALL
+
+
+computeMatrixOperations rbind -m Acclimation_1_31_5a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_less_gene26592_and_5615.bed_matrix.mat.gz Acclimation_1_31_5b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_less_gene26592_and_5615.bed_matrix.mat.gz Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_less_gene26592_and_5615.bed_matrix.mat.gz -o Acclimation_1_31_5_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz
+plotHeatmap -m Acclimation_1_31_5_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz -out  Acclimation_1_31_5_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz.pdf
+
+
+computeMatrixOperations rbind -m Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_less_gene26592_and_5615.bed_matrix.mat.gz Acclimation_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_less_gene26592_and_5615.bed_matrix.mat.gz Acclimation_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_less_gene26592_and_5615.bed_matrix.mat.gz -o Acclimation_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz
+plotHeatmap -m Acclimation_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz -out  Acclimation_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz.pdf
+
+
+computeMatrixOperations rbind -m Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_less_gene26592_and_5615.bed_matrix.mat.gz Control_1_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_less_gene26592_and_5615.bed_matrix.mat.gz Control_1_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_less_gene26592_and_5615.bed_matrix.mat.gz -o Control_1_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz
+plotHeatmap -m Control_1_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz -out  Control_1_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz.pdf
+
+
+computeMatrixOperations rbind -m Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_less_gene26592_and_5615.bed_matrix.mat.gz Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_less_gene26592_and_5615.bed_matrix.mat.gz Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_less_gene26592_and_5615.bed_matrix.mat.gz -o Control_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz
+plotHeatmap -m Control_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz -out  Control_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz.pdf
+
+
+#### Up
+
+computeMatrixOperations rbind -m Acclimation_1_31_5a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_up_less_gene26592_and_5615.bed_matrix.mat.gz Acclimation_1_31_5b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_up_less_gene26592_and_5615.bed_matrix.mat.gz Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_up_less_gene26592_and_5615.bed_matrix.mat.gz -o Acclimation_1_31_5_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz
+plotHeatmap -m Acclimation_1_31_5_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz -out  Acclimation_1_31_5_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz.pdf
+
+
+computeMatrixOperations rbind -m Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_up_less_gene26592_and_5615.bed_matrix.mat.gz Acclimation_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_up_less_gene26592_and_5615.bed_matrix.mat.gz Acclimation_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_up_less_gene26592_and_5615.bed_matrix.mat.gz -o Acclimation_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz
+plotHeatmap -m Acclimation_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz -out  Acclimation_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz.pdf
+
+
+computeMatrixOperations rbind -m Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_up_less_gene26592_and_5615.bed_matrix.mat.gz Control_1_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_up_less_gene26592_and_5615.bed_matrix.mat.gz Control_1_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_up_less_gene26592_and_5615.bed_matrix.mat.gz -o Control_1_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz
+plotHeatmap -m Control_1_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz -out  Control_1_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz.pdf
+
+
+computeMatrixOperations rbind -m Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_up_less_gene26592_and_5615.bed_matrix.mat.gz Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_up_less_gene26592_and_5615.bed_matrix.mat.gz Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bwselect_C1vsA1_p0_05_up_less_gene26592_and_5615.bed_matrix.mat.gz -o Control_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz
+plotHeatmap -m Control_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz -out  Control_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz.pdf
+
+
+
+
+################################################################################################################################################################
+################################################################################################################################################################
+# cbind pour aligner les 4 temps d'echantillonnage
+################################################################################################################################################################
+################################################################################################################################################################
+
+
+
+computeMatrixOperations cbind -m Acclimation_1_31_5_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz Acclimation_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz Control_1_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz Control_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz -o select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz 
+
+plotHeatmap -m select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz \
+     -out select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz.pdf \
      --colorMap RdBu \
      --whatToShow 'plot, heatmap and colorbar' \
      --zMin -3 --zMax 3 \
      --kmeans 4 \
-     --outFileSortedRegions select_C3vsA3_p0_05_rbind_kmeans_04_out_clusters.bed
+     --outFileSortedRegions select_C1vsA1_p0_05_up_less_gene26592_and_5615_rbind.bed_matrix.mat.gz.bed
 
+
+
+computeMatrixOperations cbind -m Acclimation_1_31_5_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz Acclimation_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz Control_1_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz Control_3_30_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz -o select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz 
+
+plotHeatmap -m select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz \
+     -out select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz.pdf \
+     --colorMap RdBu \
+     --whatToShow 'plot, heatmap and colorbar' \
+     --zMin -3 --zMax 3 \
+     --kmeans 4 \
+     --outFileSortedRegions select_C1vsA1_p0_05_less_gene26592_and_5615_rbind.bed_matrix.mat.gz.bed
 
 
 
