84405f3ade46a403ce2abb67587532731a693a63
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-07-08 16:28:15 +1100

    Create ZZ_21_selected_all_CpG_down.sh

---
+++ 00_scripts/ZZ_21_selected_all_CpG_down.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#PBS -q mpi
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=28:mem=115g
+
+
+DATADIRECTORY=/home1/scratch/plstenge/A_digitifera
+BW_DIRECTORY=/home1/datawork/plstenge/A_digitifera
+
+cd $DATADIRECTORY
+
+export PATH=$PATH:/appli/anaconda/2.7/bin
+source activate $DATAWORK/96_env_conda/deeptools
+
+cd $DATADIRECTORY
+
+
+################################################################################################################################################################
+################################################################################################################################################################
+# computeMatrix sample by sample
+################################################################################################################################################################
+################################################################################################################################################################
+
+
+
+for SELECTION in $(ls $DATADIRECTORY/CpG_*.bed)
+do
+
+for FILE in $(ls $BW_DIRECTORY/*.bam_sorted.bam.bw)
+do
+
+computeMatrix scale-regions -S ${FILE##*/} -R ${SELECTION##*/} --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o ${FILE##*/}_${SELECTION##*/}_matrix.mat.gz
+
+#plotHeatmap -m ${FILE##*/}_${SELECTION##*/}_matrix.mat.gz -out ${FILE##*/}_${SELECTION##*/}_matrix.mat.gz.pdf 
+
+done ;
+done ;
