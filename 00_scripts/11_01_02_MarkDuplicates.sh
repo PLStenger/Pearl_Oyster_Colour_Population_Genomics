ccfe29ee17a910218b72993dc15229392d323fe9
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-09-04 09:23:04 +1100

    Update 11_01_02_MarkDuplicates.sh

--- 00_scripts/11_01_02_MarkDuplicates.sh
+++ 00_scripts/11_01_02_MarkDuplicates.sh
@@ -12,6 +12,8 @@ SAMTOOLS=". /appli/bioinfo/samtools/latest/env.sh" # samtools-1.4.1
 GATK=". /appli/bioinfo/gatk/latest/env.sh" # version 4.0.2.1-0
 
 module load anaconda-py2.7/4.3.13
+module load java/1.8.0_121
+source activate /home/datawork-rmpf/p_margaritifera/pl-pwgs/98_programms/picard_tools-1.119
 
 # 1) Marking duplicates and removing them
 
