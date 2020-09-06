def892cef8d3ed3f506dadb02e5bd3251f477a38
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-09-07 10:37:48 +1100

    Update 11_01_02_MarkDuplicates.sh

--- 00_scripts/11_01_02_MarkDuplicates.sh
+++ 00_scripts/11_01_02_MarkDuplicates.sh
@@ -47,6 +47,9 @@ source activate /home/datawork-rmpf/p_margaritifera/pl-pwgs/98_programms/picard_
 cd $OUTDIR
 $GATK
 
+# Create the dictionnary
+java -jar picard.jar CreateSequenceDictionary R=Pearl_Oyster_Colour_Population_Genomics/01_genome/sspace.final.scaffolds.fasta O=Pearl_Oyster_Colour_Population_Genomics/01_genome/sspace.final.scaffolds.dict
+
 for FILE in $(ls $OUTDIR/*_sorted.bam)
 
 do
