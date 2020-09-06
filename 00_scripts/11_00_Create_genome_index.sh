aee4a125571423d877c9db29e5dcd9cfc7f9e965
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-09-07 10:27:59 +1100

    Update 11_00_Create_genome_index.sh

--- 00_scripts/11_00_Create_genome_index.sh
+++ 00_scripts/11_00_Create_genome_index.sh
@@ -3,8 +3,8 @@
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=28:mem=115g
 
-WORKING_DIRECTORY=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data
-DATA=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds.fasta
+WORKING_DIRECTORY=/home2/scratch/plstenge/bam
+DATA=/home2/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_genome/sspace.final.scaffolds.fasta
 SAMTOOLS=". /appli/bioinfo/samtools/latest/env.sh" # samtools-1.4.1
 
 cd $WORKING_DIRECTORY
