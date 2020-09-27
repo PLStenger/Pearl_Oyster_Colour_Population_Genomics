f41add3bf43d2f03657f7b40e5cba67b9b617f81
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-09-28 09:07:58 +1100

    Update 22_04_snpEff_pval_9.sh

--- 00_scripts/22_04_snpEff_pval_9.sh
+++ 00_scripts/22_04_snpEff_pval_9.sh
@@ -16,9 +16,9 @@ source activate /home/datawork-rmpf/p_margaritifera/pl-pwgs/98_programms/SNPeff/
 
 DBNAME=sspace.final.scaffolds.fasta
 
-VCF_RvsG=/home/datawork-rmpf/p_margaritifera/pl-pwgs/06_vcf/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex_header_RedVsGreen_dat_P_0_000000001.txt.vcf_SNPEff_02.vcf
-VCF_YvsG=/home/datawork-rmpf/p_margaritifera/pl-pwgs/06_vcf/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex_header_YellowVsGreen_dat_P_0_000000001.txt.vcf_SNPEff_02.vcf
-VCF_YvsR=/home/datawork-rmpf/p_margaritifera/pl-pwgs/06_vcf/individuals.vcf_DP20_maf0.1_miss1.vcf.recode_bcfm2M2v.vcf_decomposed_complex_header_YellowVsRed_dat_P_0_000000001.txt.vcf_SNPEff_02.vcf
+VCF_RvsG=/home/datawork-rmpf/p_margaritifera/pl-pwgs/06_vcf/individuals.vcf_DP20_maf0.1_miss1_recode_bcfm2M2v_decomposed_complex_RvsG_P_0_000000001.vcf.recode_02.vcf
+VCF_YvsG=/home/datawork-rmpf/p_margaritifera/pl-pwgs/06_vcf/individuals.vcf_DP20_maf0.1_miss1_recode_bcfm2M2v_decomposed_complex_YvsG_P_0_000000001.vcf.recode_02.vcf
+VCF_YvsR=/home/datawork-rmpf/p_margaritifera/pl-pwgs/06_vcf/individuals.vcf_DP20_maf0.1_miss1_recode_bcfm2M2v_decomposed_complex_YvsR_P_0_000000001.vcf.recode_02.vcf
 
 WORKDIR=/home/datawork-rmpf/p_margaritifera/pl-pwgs/07_snpeff
 
