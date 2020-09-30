636fab3bf9264f9271d0c857fc2ba762a8c9a3ab
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-09-30 17:11:19 +1100

    Update 99_find_blast_specific_color_SNPs.sh

--- 00_scripts/99_find_blast_specific_color_SNPs.sh
+++ 00_scripts/99_find_blast_specific_color_SNPs.sh
@@ -9,9 +9,9 @@ BEDTOOLS_ENV=". /appli/bioinfo/bedtools/2.27.1/env.sh" # https://bedtools.readth
 
 $BEDTOOLS_ENV
 
-sed $'s\_\/t\' Green_specific_SNPs_P_0_000000001_list.txt > Green_specific_SNPs_P_0_000000001_list_02.txt
-sed $'s\_\/t\' Red_specific_SNPs_P_0_000000001_list.txt > Red_specific_SNPs_P_0_000000001_list_02.txt
-sed $'s\_\/t\' Yellow_specific_SNPs_P_0_000000001_list.txt > Yellow_specific_SNPs_P_0_000000001_list_02.txt
+awk -vRS="_" -vORS="\t" '1'  Green_specific_SNPs_P_0_000000001_list.txt > Green_specific_SNPs_P_0_000000001_list_02.txt
+awk -vRS="_" -vORS="\t" '1'  Red_specific_SNPs_P_0_000000001_list.txt > Red_specific_SNPs_P_0_000000001_list_02.txt
+awk -vRS="_" -vORS="\t" '1'  Yellow_specific_SNPs_P_0_000000001_list.txt > Yellow_specific_SNPs_P_0_000000001_list_02.txt
 
 awk -v OFS='/t' '{print $1, $2, $2}' Green_specific_SNPs_P_0_000000001_list_02.txt > Green_specific_SNPs_P_0_000000001_list_03.txt
 awk -v OFS='/t' '{print $1, $2, $2}' Red_specific_SNPs_P_0_000000001_list_02.txt > Red_specific_SNPs_P_0_000000001_list_03.txt
