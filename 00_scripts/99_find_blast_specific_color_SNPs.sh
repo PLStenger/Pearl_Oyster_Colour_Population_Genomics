dd1c5aced5c80c25aef0f6b21d6a9e0e8323197a
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-09-30 17:01:53 +1100

    Create 99_find_blast_specific_color_SNPs.sh

---
+++ 00_scripts/99_find_blast_specific_color_SNPs.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#PBS -q mpi
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=28:mem=115g
+
+cd /home/datawork-rmpf/p_margaritifera/pl-pwgs/07_snpeff
+
+BEDTOOLS_ENV=". /appli/bioinfo/bedtools/2.27.1/env.sh" # https://bedtools.readthedocs.io/en/latest/content/tools/getfasta.html
+
+cd $DATADIRECTORY
+
+$BEDTOOLS_ENV'
+
+sed $'s\_\/t\g' Green_specific_SNPs_P_0_000000001_list.txt > Green_specific_SNPs_P_0_000000001_list_02.txt
+sed $'s\_\/t\g' Red_specific_SNPs_P_0_000000001_list.txt > Red_specific_SNPs_P_0_000000001_list_02.txt
+sed $'s\_\/t\g' Yellow_specific_SNPs_P_0_000000001_list.txt > Yellow_specific_SNPs_P_0_000000001_list_02.txt
+
+awk -v OFS='/t' '{print $1, $2, $2}' Green_specific_SNPs_P_0_000000001_list_02.txt > Green_specific_SNPs_P_0_000000001_list_03.txt
+awk -v OFS='/t' '{print $1, $2, $2}' Red_specific_SNPs_P_0_000000001_list_02.txt > Red_specific_SNPs_P_0_000000001_list_03.txt
+awk -v OFS='/t' '{print $1, $2, $2}' Yellow_specific_SNPs_P_0_000000001_list_02.txt > Yellow_specific_SNPs_P_0_000000001_list_03.txt
+
+
+bedtools intersect -b Green_specific_SNPs_P_0_000000001_list_03.txt -a /home/datawork-rmpf/p_margaritifera/genome/annotation/EVM_combined.gff3 -wb -wa > Green_specific_SNPs_P_0_000000001_list_03_gene.txt
+bedtools intersect -b Red_specific_SNPs_P_0_000000001_list_03.txt -a /home/datawork-rmpf/p_margaritifera/genome/annotation/EVM_combined.gff3 -wb -wa > Red_specific_SNPs_P_0_000000001_list_03_gene.txt
+bedtools intersect -b Yellow_specific_SNPs_P_0_000000001_list_03.txt -a /home/datawork-rmpf/p_margaritifera/genome/annotation/EVM_combined.gff3 -wb -wa > Yellow_specific_SNPs_P_0_000000001_list_03_gene.txt
+
+
